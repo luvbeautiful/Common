578f0cfc64655894c99697e7edbb5491ddaf4269
Zypppy <zypppaliukas@gmail.com>
2015-08-08 05:21:12 +0300

    Create Olaf.lua

---
+++ Olaf.lua
@@ -0,0 +1,38 @@
+Config = scriptConfig("Olaf", "Olaf:")
+Config.addParam("Q", "Use Q", SCRIPT_PARAM_ONOFF, true)
+Config.addParam("W", "Use W", SCRIPT_PARAM_ONOFF, true)
+Config.addParam("E", "Use E", SCRIPT_PARAM_ONOFF, true)
+DrawingsConfig = scriptConfig("Drawings", "Drawings:")
+DrawingsConfig.addParam("DrawQ","Draw Q", SCRIPT_PARAM_ONOFF, true)
+
+
+ 
+ 
+myIAC = IAC()
+ 
+OnLoop(function(myHero)
+Drawings()
+
+ 
+        if IWalkConfig.Combo then
+              local target = GetTarget(1000, DAMAGE_PHYSICAL)
+                if ValidTarget(target, 1000) then
+                       
+                        local QPred = GetPredictionForPlayer(GetMyHeroPos(),target,GetMoveSpeed(target),1000,250,GetCastRange(myHero,_Q),105,false,false)
+                        if CanUseSpell(myHero, _Q) == READY and QPred.HitChance == 1 and ValidTarget(target, GetCastRange(myHero,_Q)) and Config.Q then
+                        CastSkillShot(_Q,QPred.PredPos.x,QPred.PredPos.y,QPred.PredPos.z)
+						end
+						if CanUseSpell(myHero, _W) == READY and ValidTarget(target, GetCastRange(myHero,_W)) and IsInDistance(target, 200) and Config.W then
+                        CastSpell(_W)
+						end
+						if CanUseSpell(myHero, _E) == READY and ValidTarget(target, GetCastRange(myHero,_E)) and IsInDistance(target, 325) and Config.E then
+						CastTargetSpell(target, _E)
+						end
+                end
+        end
+end)
+ 
+function Drawings()
+myHeroPos = GetOrigin(myHero)
+if CanUseSpell(myHero, _Q) == READY and DrawingsConfig.DrawQ then DrawCircle(myHeroPos.x,myHeroPos.y,myHeroPos.z,GetCastRange(myHero,_Q),3,100,0xff00ff00) end
+end
