1f2050ad07ed8de534c8b3401da826e8c18e20db
Zypppy <zypppaliukas@gmail.com>
2015-08-08 11:40:07 +0300

    Added auto R ks  function

--- ChoGat.lua
+++ ChoGat.lua
@@ -3,7 +3,7 @@ Config.addParam("Q", "Use Q", SCRIPT_PARAM_ONOFF, true)
 Config.addParam("W", "Use W", SCRIPT_PARAM_ONOFF, true)
 Config.addParam("R", "Use R", SCRIPT_PARAM_ONOFF, true)
 KSConfig = scriptConfig("KS", "Killsteal:")
-KSConfig.addParam("KSQ", "Killsteal with Q", SCRIPT_PARAM_ONOFF, false)
+KSConfig.addParam("KSR", "Killsteal with R", SCRIPT_PARAM_ONOFF, false)
 DrawingsConfig = scriptConfig("Drawings", "Drawings:")
 DrawingsConfig.addParam("DrawQ","Draw Q", SCRIPT_PARAM_ONOFF, true)
  
@@ -31,20 +31,6 @@ Killsteal()
                         CastTargetSpell(target, _R)
 						end
                 end
-        end    
-       
-        if IWalkConfig.Harass then
-             local target = GetTarget(1000, DAMAGE_MAGIC)
-                             
-                if ValidTarget(target, 1000) then
-               
-                        if (GetCurrentMana(myHero)/GetMaxMana(myHero)) > 0.4 then
-                                local QPred = GetPredictionForPlayer(GetMyHeroPos(),target,GetMoveSpeed(target),1200,250,GetCastRange(myHero,_Q),80,true,true)
-                                if CanUseSpell(myHero, _Q) == READY and QPred.HitChance == 1 and ValidTarget(target,GetCastRange(myHero,_Q)) and HarassConfig.HarassQ then
-                                CastSkillShot(_Q,QPred.PredPos.x,QPred.PredPos.y,QPred.PredPos.z)
-                                end
-                        end
-                end
         end
 end)
  
@@ -55,11 +41,10 @@ end)
  
 function Killsteal()
         for i,enemy in pairs(GetEnemyHeroes()) do
-            local QPred = GetPredictionForPlayer(GetMyHeroPos(),target,GetMoveSpeed(target),1200,250,GetCastRange(myHero,_Q),80,true,true)
-            if CanUseSpell(myHero, _Q) == READY and QPred.HitChance == 1 and ValidTarget(enemy,GetCastRange(myHero,_Q)) and KSConfig.KSQ and GetCurrentHP(enemy) < CalcDamage(myHero, enemy, 0, (50*GetCastLevel(myHero,_Q)+10+GetBonusAP(myHero))) then
-            CastSkillShot(_Q,QPred.PredPos.x,QPred.PredPos.y,QPred.PredPos.z)
+            if CanUseSpell(myHero, _R) == READY and ValidTarget(enemy,GetCastRange(myHero,_R)) and KSConfig.KSR and GetCurrentHP(enemy) < CalcDamage(myHero, enemy, 0, (175*GetCastLevel(myHero,_R)+70+GetBonusAP(myHero))) then
+            CastTargetSpell(target, _R)
             end
-        end
+      end
 end
  
 function Drawings()
