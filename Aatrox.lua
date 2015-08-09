d8937ef75dc1cfccd52a72541f0934bd20737eaa
Zypppy <zypppaliukas@gmail.com>
2015-08-09 13:59:18 +0300

    Create Aatrox.lua
    QE , For now

---
+++ Aatrox.lua
@@ -0,0 +1,41 @@
+Config = scriptConfig("Aatrox", "Aatrox:")
+Config.addParam("Q", "Use Q", SCRIPT_PARAM_ONOFF, true)
+Config.addParam("E", "Use E", SCRIPT_PARAM_ONOFF, true)
+Config.addParam("R", "Use R", SCRIPT_PARAM_ONOFF, true)
+DrawingsConfig = scriptConfig("Drawings", "Drawings:")
+DrawingsConfig.addParam("DrawQ","Draw Q", SCRIPT_PARAM_ONOFF, true)
+DrawingsConfig.addParam("DrawE","Draw E", SCRIPT_PARAM_ONOFF, true)
+
+
+ 
+ 
+myIAC = IAC()
+ 
+OnLoop(function(myHero)
+Drawings()
+ 
+        if IWalkConfig.Combo then
+              local target = GetTarget(1000, DAMAGE_PHYSICAL)
+                if ValidTarget(target, 1000) then
+				
+				        local QPred = GetPredictionForPlayer(GetMyHeroPos(),target,GetMoveSpeed(target),650,600,GetCastRange(myHero,_Q),250,false,false)
+                        if CanUseSpell(myHero, _Q) == READY and QPred.HitChance == 1 and ValidTarget(target, GetCastRange(myHero,_Q)) and Config.Q then
+                        CastSkillShot(_Q,QPred.PredPos.x,QPred.PredPos.y,QPred.PredPos.z)
+						end
+						local EPred = GetPredictionForPlayer(GetMyHeroPos(),target,GetMoveSpeed(target),1075,250,GetCastRange(myHero,_E),35,false,false)
+                        if CanUseSpell(myHero, _E) == READY and EPred.HitChance == 1 and ValidTarget(target, GetCastRange(myHero,_E)) and Config.E then
+                        CastSkillShot(_E,EPred.PredPos.x,EPred.PredPos.y,EPred.PredPos.z)
+						end
+						if CanUseSpell(myHero, _R) == READY and ValidTarget(target, GetCastRange(myHero,_R)) and IsInDistance(target, 500) and Config.R then
+						CastSpell(_R)
+						end
+                end
+        end
+end)
+
+ 
+function Drawings()
+myHeroPos = GetOrigin(myHero)
+if CanUseSpell(myHero, _Q) == READY and DrawingsConfig.DrawQ then DrawCircle(myHeroPos.x,myHeroPos.y,myHeroPos.z,GetCastRange(myHero,_Q),3,100,0xff00ff000) end
+if CanUseSpell(myHero, _E) == READY and DrawingsConfig.DrawE then DrawCircle(myHeroPos.x,myHeroPos.y,myHeroPos.z,GetCastRange(myHero,_E),3,100,0xff00fc00) end
+end
