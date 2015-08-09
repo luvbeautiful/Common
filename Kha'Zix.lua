9131c5afce3e9b29846e2bf0e1b8fb20fc77ec4b
Zypppy <zypppaliukas@gmail.com>
2015-08-09 22:21:06 +0300

    Update Kha'Zix.lua

--- Kha'Zix.lua
+++ Kha'Zix.lua
@@ -17,17 +17,17 @@ Drawings()
 
  
         if IWalkConfig.Combo then
-              local target = GetTarget(1000, DAMAGE_PHYSICAL)
-                if ValidTarget(target, 1000) then
+              local target = GetTarget(800, DAMAGE_PHYSICAL)
+                if ValidTarget(target, 800) then
                        
                         if CanUseSpell(myHero, _Q) == READY and ValidTarget(target, GetCastRange(myHero,_Q)) and IsInDistance(target, 420) and Config.Q then
                         CastTargetSpell(target, _Q)
                         end
-						local WPred = GetPredictionForPlayer(GetMyHeroPos(),target,GetMoveSpeed(target),1025,250,GetCastRange(myHero,_W),73,true,true)
+						local WPred = GetPredictionForPlayer(GetMyHeroPos(),target,GetMoveSpeed(target),1025,500,GetCastRange(myHero,_W),73,true,true)
                         if CanUseSpell(myHero, _W) == READY and WPred.HitChance == 1 and ValidTarget(target, GetCastRange(myHero,_W)) and Config.W then
                         CastSkillShot(_W,WPred.PredPos.x,WPred.PredPos.y,WPred.PredPos.z)
 						end
-						local EPred = GetPredictionForPlayer(GetMyHeroPos(),target,GetMoveSpeed(target),900,250,GetCastRange(myHero,_E),300,false,false)
+						local EPred = GetPredictionForPlayer(GetMyHeroPos(),target,GetMoveSpeed(target),900,500,GetCastRange(myHero,_E),300,false,false)
                         if CanUseSpell(myHero, _E) == READY and EPred.HitChance == 1 and ValidTarget(target, GetCastRange(myHero,_E)) and Config.E then
                         CastSkillShot(_E,EPred.PredPos.x,EPred.PredPos.y,EPred.PredPos.z)
 						end
