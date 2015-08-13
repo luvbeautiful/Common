cacb0e31e6ead2269470723245e1e3b2adfc7657
Zypppy <zypppaliukas@gmail.com>
2015-08-13 20:09:46 +0300

    Update Master Yi.lua
    whoops forgot to add  R

--- Master Yi.lua
+++ Master Yi.lua
@@ -17,7 +17,8 @@ Drawings()
 					    if CanUseSpell(myHero, _Q) == READY and ValidTarget(target, GetCastRange(myHero,_Q)) and IsInDistance(target, 600) and Config.Q then
                         CastTargetSpell(target, _Q)
                         end
-                        
+                        if CanUseSpell(myHero, _R) == READY and ValidTarget(target, GetCastRange(myHero,_R)) and IsInDistance(target, 300) and Config.R then
+                        CastSpell(_R)
                         end
 						if CanUseSpell(myHero, _E) == READY and ValidTarget(target, GetCastRange(myHero,_E)) and IsInDistance(target, 170) and Config.E then
                         CastSpell(_E)
