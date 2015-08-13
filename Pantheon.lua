8689d83b59431589d1a7d314dd2ffe87b254510d
Zypppy <zypppaliukas@gmail.com>
2015-08-14 00:02:25 +0300

    Update Pantheon.lua

--- Pantheon.lua
+++ Pantheon.lua
@@ -1,8 +1,6 @@
 Config = scriptConfig("Pantheon", "Pantheon:")
 Config.addParam("Q", "Use Q", SCRIPT_PARAM_ONOFF, true)
 Config.addParam("W", "Use W", SCRIPT_PARAM_ONOFF, true)
-KSConfig = scriptConfig("KS", "Killsteal:")
-KSConfig.addParam("KSQ", "Killsteal with Q", SCRIPT_PARAM_ONOFF, true)
 DrawingsConfig = scriptConfig("Drawings", "Drawings:")
 DrawingsConfig.addParam("DrawQ","Draw Q", SCRIPT_PARAM_ONOFF, true)
 
@@ -13,11 +11,11 @@ myIAC = IAC()
  
 OnLoop(function(myHero)
 Drawings()
-Killsteal()
+
  
         if IWalkConfig.Combo then
-              local target = GetTarget(600, DAMAGE_PHYSICAL)
-                if ValidTarget(target, 600) then
+              local target = GetTarget(1000, DAMAGE_PHYSICAL)
+                if ValidTarget(target, 1000) then
 				
 				        if CanUseSpell(myHero, _Q) == READY and ValidTarget(target, GetCastRange(myHero,_Q)) and IsInDistance(target, 600) and Config.Q then
                         CastTargetSpell(target, _Q)
@@ -28,14 +26,6 @@ Killsteal()
                 end
         end
 end)
-
-function Killsteal()
-	for i,enemy in pairs(GetEnemyHeroes()) do
-                 if CanUseSpell(myHero,_Q) and ValidTarget(enemy, GetCastRange(myHero,_Q)) and KSConfig.KSQ and GetCurrentHP(enemy) < CalcDamage(myHero, enemy, 0, (40*GetCastLevel(myHero,_Q) + 0.90*GetBonusDmg(myHero))) then
-                 CastTargetSpell(enemy, _Q)
-            end
-      end
-end
  
 function Drawings()
 myHeroPos = GetOrigin(myHero)
