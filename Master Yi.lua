f31b82c41f799ad153fa0d416117112bccd833fb
Zypppy <zypppaliukas@gmail.com>
2015-08-14 00:01:49 +0300

    Update Master Yi.lua

--- Master Yi.lua
+++ Master Yi.lua
@@ -1,43 +1,31 @@
-Config = scriptConfig("Master Yi", "Master Yi:")
+Config = scriptConfig("Pantheon", "Pantheon:")
 Config.addParam("Q", "Use Q", SCRIPT_PARAM_ONOFF, true)
-Config.addParam("E", "Use E", SCRIPT_PARAM_ONOFF, true)
-Config.addParam("R", "Use R", SCRIPT_PARAM_ONOFF, true)
-KSConfig = scriptConfig("KS", "Killsteal:")
-KSConfig.addParam("KSQ", "Killsteal with Q", SCRIPT_PARAM_ONOFF, true)
+Config.addParam("W", "Use W", SCRIPT_PARAM_ONOFF, true)
 DrawingsConfig = scriptConfig("Drawings", "Drawings:")
 DrawingsConfig.addParam("DrawQ","Draw Q", SCRIPT_PARAM_ONOFF, true)
+
+
+ 
  
 myIAC = IAC()
  
 OnLoop(function(myHero)
 Drawings()
-Killsteal()
+
  
         if IWalkConfig.Combo then
               local target = GetTarget(1000, DAMAGE_PHYSICAL)
                 if ValidTarget(target, 1000) then
-                       
-					    if CanUseSpell(myHero, _Q) == READY and ValidTarget(target, GetCastRange(myHero,_Q)) and IsInDistance(target, 600) and Config.Q then
+				
+				        if CanUseSpell(myHero, _Q) == READY and ValidTarget(target, GetCastRange(myHero,_Q)) and IsInDistance(target, 600) and Config.Q then
                         CastTargetSpell(target, _Q)
                         end
-                        if CanUseSpell(myHero, _R) == READY and ValidTarget(target, GetCastRange(myHero,_R)) and IsInDistance(target, 300) and Config.R then
-                        CastSpell(_R)
-                        end
-						if CanUseSpell(myHero, _E) == READY and ValidTarget(target, GetCastRange(myHero,_E)) and IsInDistance(target, 170) and Config.E then
-                        CastSpell(_E)
+						if CanUseSpell(myHero, _W) == READY and ValidTarget(target, GetCastRange(myHero,_W)) and IsInDistance(target, 598) and Config.W then
+                        CastTargetSpell(target, _W)
 						end
                 end
         end
 end)
-
-function Killsteal()
-	for i,enemy in pairs(GetEnemyHeroes()) do
-                 if CanUseSpell(myHero,_Q) and ValidTarget(enemy, GetCastRange(myHero,_Q)) and KSConfig.KSQ and GetCurrentHP(enemy) < CalcDamage(myHero, enemy, 0, (35*GetCastLevel(myHero,_Q) + 100*GetBonusDmg(myHero))) then
-                 CastTargetSpell(enemy, _Q)
-            end
-      end
-end 
-
  
 function Drawings()
 myHeroPos = GetOrigin(myHero)
