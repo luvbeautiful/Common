335d241bb3debf1943bb96977a325c5b65a15aa2
Zypppy <zypppaliukas@gmail.com>
2015-08-17 17:18:38 +0300

    Update Ahri.lua

--- Ahri.lua
+++ Ahri.lua
@@ -10,38 +10,38 @@ DrawingsConfig.addParam("DrawE","Draw E", SCRIPT_PARAM_ONOFF, true)
 DrawingsConfig.addParam("DrawR","Draw R", SCRIPT_PARAM_ONOFF, true)
 
 
- 
- 
+ 
+ 
 myIAC = IAC()
- 
+ 
 OnLoop(function(myHero)
 Drawings()
 
- 
-        if IWalkConfig.Combo then
-              local target = GetTarget(1000, DAMAGE_MAGICAL)
- local mousePos = GetMousePos()
-                if ValidTarget(target, 1000) then
-                       
-                        local RPred = GetPredictionForPlayer(GetMyHeroPos(),target,GetMoveSpeed(target),500,0,GetCastRange(myHero,_R),250,false,false)
-                        if CanUseSpell(myHero, _R) == READY and RPred.HitChance == 1 and ValidTarget(target, GetCastRange(myHero,_R)) and Config.R then
-                        CastSkillShot(_R, mousePos.x, mousePos.y, mousePos.z)
+ 
+        if IWalkConfig.Combo then
+              local target = GetTarget(1000, DAMAGE_MAGICAL)
+ local mousePos = GetMousePos()
+                if ValidTarget(target, 1000) then
+                       
+                        local RPred = GetPredictionForPlayer(GetMyHeroPos(),target,GetMoveSpeed(target),500,0,GetCastRange(myHero,_R),250,false,false)
+                        if CanUseSpell(myHero, _R) == READY and RPred.HitChance == 1 and ValidTarget(target, GetCastRange(myHero,_R)) and Config.R then
+                        CastSkillShot(_R, mousePos.x, mousePos.y, mousePos.z)
 						end
 						if CanUseSpell(myHero, _W) == READY and ValidTarget(target, GetCastRange(myHero,_W)) and Config.W then
-                        CastSpell(_W)
+                        CastSpell(_W)
 						end
-local EPred = GetPredictionForPlayer(GetMyHeroPos(),target,GetMoveSpeed(target),1550,300,1000,60,true,true)
-                        if CanUseSpell(myHero, _E) == READY and EPred.HitChance == 1 and ValidTarget(target, GetCastRange(myHero,_E)) and Config.E then
-                        CastSkillShot(_E,EPred.PredPos.x,EPred.PredPos.y,EPred.PredPos.z)
-end
-					local QPred = GetPredictionForPlayer(2500,300,1000,100,true,true)
-                        if CanUseSpell(myHero, _Q) == READY and QPred.HitChance == 1 and ValidTarget(target, GetCastRange(myHero,_Q)) and Config.Q then
-                        CastSkillShot(_Q,QPred.PredPos.x,QPred.PredPos.y,QPred.PredPos.z)
+                        local EPred = GetPredictionForPlayer(GetMyHeroPos(),target,GetMoveSpeed(target),1550,300,1000,60,true,true)
+                        if CanUseSpell(myHero, _E) == READY and EPred.HitChance == 1 and ValidTarget(target, GetCastRange(myHero,_E)) and Config.E then
+                        CastSkillShot(_E,EPred.PredPos.x,EPred.PredPos.y,EPred.PredPos.z)
+                        end
+					   local QPred = GetPredictionForPlayer(GetMyHeroPos(),target,GetMoveSpeed(target),2500,300,1000,100,false,false)
+                        if CanUseSpell(myHero, _Q) == READY and QPred.HitChance == 1 and ValidTarget(target, GetCastRange(myHero,_Q)) and Config.Q then
+                        CastSkillShot(_Q,QPred.PredPos.x,QPred.PredPos.y,QPred.PredPos.z)
 						end
-                end
-        end
+                end
+        end
 end)
- 
+ 
 function Drawings()
 myHeroPos = GetOrigin(myHero)
 if CanUseSpell(myHero, _Q) == READY and DrawingsConfig.DrawQ then DrawCircle(myHeroPos.x,myHeroPos.y,myHeroPos.z,GetCastRange(myHero,_Q),3,100,0xff00ff00) end
