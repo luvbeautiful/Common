77138750e459477108d31ddd5aa76764967a5eae
Zypppy <zypppaliukas@gmail.com>
2015-08-17 16:20:58 +0300

    Update Karthus.lua

--- Karthus.lua
+++ Karthus.lua
@@ -1,6 +1,6 @@
 Config = scriptConfig("Karthus", "Karthus:")
 Config.addParam("Q", "Use Q", SCRIPT_PARAM_ONOFF, true)
-Config.addParam("R", "Use R", SCRIPT_PARAM_ONOFF, true)
+Config.addParam("E", "Use E", SCRIPT_PARAM_ONOFF, true)
 KSConfig = scriptConfig("KS", "Killsteal:")
 KSConfig.addParam("KSR", "Killsteal with R", SCRIPT_PARAM_ONOFF, true)
 DrawingsConfig = scriptConfig("Drawings", "Drawings:")
@@ -21,6 +21,11 @@ Killsteal()
                         if CanUseSpell(myHero, _Q) == READY and QPred.HitChance == 1 and ValidTarget(target, GetCastRange(myHero,_Q)) and Config.Q then
                         CastSkillShot(_Q,QPred.PredPos.x,QPred.PredPos.y,QPred.PredPos.z)
 						end
+						if GotBuff(myHero, "KarthusDefile") ~= 1 then
+						if CanUseSpell(myHero, _E) == READY and ValidTarget(target, GetCastRange(myHero,_E)) and Config.E then
+                        CastSpell(_E)
+						end
+						end
                 end
         end
 end)
@@ -28,6 +33,7 @@ end)
 function Killsteal()
 	for i,enemy in pairs(GetEnemyHeroes()) do
                  if CanUseSpell(myHero, _R) == READY and ValidTarget(enemy, GetCastRange(myHero,_R)) and KSConfig.KSR and GetCurrentHP(enemy) < CalcDamage(myHero, enemy, 0, (150*GetCastLevel(myHero,_R) + 60 + 0.60*GetBonusAP(myHero))) then
+				 waitTickCount = GetTickCount() + 3000
                  CastSpell(_R)
             end
       end
