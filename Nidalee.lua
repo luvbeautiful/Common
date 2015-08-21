8038363ba7ff664ce2c5b495e79e8d448848e04f
Zypppy <zypppaliukas@gmail.com>
2015-08-21 19:09:55 +0300

    Update Nidalee.lua

--- Nidalee.lua
+++ Nidalee.lua
@@ -16,7 +16,7 @@ Drawings()
               local target = GetTarget(1200, DAMAGE_MAGIC)
                 if ValidTarget(target, 1200) then
                        
-                        local QPred = GetPredictionForPlayer(GetMyHeroPos(),target,GetMoveSpeed(target),1300,200,1500,40,true,true)
+                        local QPred = GetPredictionForPlayer(GetMyHeroPos(),target,GetMoveSpeed(target),1300,200,1500,40,true,false)
                         if CanUseSpell(myHero, _Q) == READY and QPred.HitChance == 1 and ValidTarget(target, GetCastRange(myHero,_Q)) and Config.Q then
                         CastSkillShot(_Q,QPred.PredPos.x,QPred.PredPos.y,QPred.PredPos.z)
 						end
