b0cdec5d73e34cfdae77ce0cd7b0c155dea91b78
Zypppy <zypppaliukas@gmail.com>
2015-08-30 13:54:02 +0300

    Update Gragas.lua

--- Gragas.lua
+++ Gragas.lua
@@ -9,7 +9,7 @@ DrawingsConfig = scriptConfig("Drawings", "Drawings:")
 DrawingsConfig.addParam("DrawQ","Draw Q", SCRIPT_PARAM_ONOFF, true)
 DrawingsConfig.addParam("DrawE","Draw E", SCRIPT_PARAM_ONOFF, true)
 DrawingsConfig.addParam("DrawR","Draw R", SCRIPT_PARAM_ONOFF, true)
-
+Config.addParam("DMG", "DMG", SCRIPT_PARAM_ONOFF, true)
  
  
 myIAC = IAC()
@@ -17,13 +17,13 @@ myIAC = IAC()
 OnLoop(function(myHero)
 Drawings()
 Killsteal()
-
+local target = GetCurrentTarget()
  
         if IWalkConfig.Combo then
               local target = GetTarget(1300, DAMAGE_MAGIC)
                 if ValidTarget(target, 1300) then
                        
-                        local QPred = GetPredictionForPlayer(GetMyHeroPos(),target,GetMoveSpeed(target),1300,300,1100,275,false,true)
+                        local QPred = GetPredictionForPlayer(GetMyHeroPos(),target,GetMoveSpeed(target),1000,300,1100,275,false,true)
                         if CanUseSpell(myHero, _Q) == READY and QPred.HitChance == 1 and ValidTarget(target, GetCastRange(myHero,_Q)) and Config.Q then
                         CastSkillShot(_Q,QPred.PredPos.x,QPred.PredPos.y,QPred.PredPos.z)
                         end
@@ -34,18 +34,44 @@ Killsteal()
                         if CanUseSpell(myHero, _E) == READY and EPred.HitChance == 1 and ValidTarget(target, GetCastRange(myHero,_E)) and Config.E then
                         CastSkillShot(_E,EPred.PredPos.x,EPred.PredPos.y,EPred.PredPos.z)
 						end
-						local RPred = GetPredictionForPlayer(GetMyHeroPos(),target,GetMoveSpeed(target),1800,300,1050,375,false,true)
+						local RPred = GetPredictionForPlayer(GetMyHeroPos(),target,GetMoveSpeed(target),1750,250,1050,350,false,true)
                         if CanUseSpell(myHero, _R) == READY and QPred.HitChance == 1 and ValidTarget(target, GetCastRange(myHero,_R)) and Config.R then
                         CastSkillShot(_R,RPred.PredPos.x,RPred.PredPos.y,RPred.PredPos.z)
 						end
                 end
         end
+
+        if ValidTarget(target, 2000) and Config.DMG then
+  if CanUseSpell(myHero,_Q) == READY then
+local trueDMG = CalcDamage(myHero, target, 0, (28*GetCastLevel(myHero,_Q) + 28 + 0.42*(GetBonusAP(myHero))))
+    DrawDmgOverHpBar(target,GetCurrentHP(target),trueDMG,0,0xff0cff00)
+    end
+
+    if CanUseSpell(myHero,_W) == READY then 
+local trueDMG = CalcDamage(myHero, target, 0, (30*GetCastLevel(myHero,_W) + 10 + 0.3*(GetBonusAP(myHero))))
+    DrawDmgOverHpBar(target,GetCurrentHP(target),trueDMG,0,0xff0cff00)
+    end
+
+    if CanUseSpell(myHero,_E) == READY then 
+local trueDMG = CalcDamage(myHero, target, 0, (50*GetCastLevel(myHero,_E) + 30 + 0.6*(GetBonusAP(myHero))))
+    DrawDmgOverHpBar(target,GetCurrentHP(target),trueDMG,0,0xff0cff00)
+  end
+
+    if CanUseSpell(myHero,_R) == READY then 
+local trueDMG = CalcDamage(myHero, target, 0, (100*GetCastLevel(myHero,_R) + 100 + 0.7*(GetBonusAP(myHero))))
+    DrawDmgOverHpBar(target,GetCurrentHP(target),trueDMG,0,0xff0cff00)
+    end
+    
+end
+
+
+     
 end)
 
 function Killsteal()
         for i,enemy in pairs(GetEnemyHeroes()) do
-	           local RPred = GetPredictionForPlayer(GetMyHeroPos(),enemy,GetMoveSpeed(enemy),0,1200,3500,190,false,true)
-  	           if CanUseSpell(myHero, _R) == READY and RPred.HitChance == 1 and ValidTarget(enemy,GetCastRange(myHero,_R)) and KSConfig.KSR and GetCurrentHP(enemy) < CalcDamage(myHero,  enemy, 0, (100*GetCastLevel(myHero,_R)+70+GetBonusAP(myHero))) then 
+	           local RPred = GetPredictionForPlayer(GetMyHeroPos(),enemy,GetMoveSpeed(enemy),1750,250,1050,350,false,true)
+  	           if CanUseSpell(myHero, _R) == READY and RPred.HitChance == 1 and ValidTarget(enemy,GetCastRange(myHero,_R)) and KSConfig.KSR and GetCurrentHP(enemy) < CalcDamage(myHero,  enemy, 0, (100*GetCastLevel(myHero,_R) + 100 + 0.7*(GetBonusAP(myHero))) then 
               CastSkillShot(_R,RPred.PredPos.x,RPred.PredPos.y,RPred.PredPos.z)
              end
       end
