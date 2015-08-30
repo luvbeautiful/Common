f224cd00662bcfbeaea4da8b192276a883579f0d
Zypppy <zypppaliukas@gmail.com>
2015-08-30 17:04:55 +0300

    Update Nidalee.lua

--- Nidalee.lua
+++ Nidalee.lua
@@ -2,7 +2,7 @@ Config = scriptConfig("Nidalee", "Nidalee:")
 Config.addParam("Q", "Use Q", SCRIPT_PARAM_ONOFF, true)
 DrawingsConfig = scriptConfig("Drawings", "Drawings:")
 DrawingsConfig.addParam("DrawQ","Draw Q", SCRIPT_PARAM_ONOFF, true)
-
+Config.addParam("DMG", "DMG", SCRIPT_PARAM_ONOFF, true)
 
  
  
@@ -10,18 +10,28 @@ myIAC = IAC()
  
 OnLoop(function(myHero)
 Drawings()
-
+local target = GetCurrentTarget()
  
         if IWalkConfig.Combo then
               local target = GetTarget(1250, DAMAGE_MAGIC)
                 if ValidTarget(target, 1250) then
                        
-                        local QPred = GetPredictionForPlayer(GetMyHeroPos(),target,GetMoveSpeed(target),1300,200,1500,40,true,false)
+                        local QPred = GetPredictionForPlayer(GetMyHeroPos(),target,GetMoveSpeed(target),1300,250,1500,60,true,false)
                         if CanUseSpell(myHero, _Q) == READY and QPred.HitChance == 1 and ValidTarget(target, GetCastRange(myHero,_Q)) and Config.Q then
                         CastSkillShot(_Q,QPred.PredPos.x,QPred.PredPos.y,QPred.PredPos.z)
 						end
                 end
         end
+
+        if ValidTarget(target, 2000) and Config.DMG then
+  if CanUseSpell(myHero,_Q) == READY then
+local trueDMG = CalcDamage(myHero, target, 0, (25*GetCastLevel(myHero,_Q) + 25 + 0.4*(GetBonusAP(myHero))))
+    DrawDmgOverHpBar(target,GetCurrentHP(target),trueDMG,0,0xff0cff00)
+    end
+
+ end
+ 
+
 end)
  
 function Drawings()
