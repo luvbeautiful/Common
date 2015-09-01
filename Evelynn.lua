7943aa8ae61044246082434c3db38b2e08ba4e0f
Zypppy <zypppaliukas@gmail.com>
2015-09-01 22:41:13 +0300

    Update Evelynn.lua

--- Evelynn.lua
+++ Evelynn.lua
@@ -6,7 +6,6 @@ Config.addParam("R", "Use R", SCRIPT_PARAM_ONOFF, true)
 DrawingsConfig = scriptConfig("Drawings", "Drawings:")
 DrawingsConfig.addParam("DrawQ","Draw Q", SCRIPT_PARAM_ONOFF, true)
 DrawingsConfig.addParam("DrawE","Draw E", SCRIPT_PARAM_ONOFF, true)
-DrawingsConfig.addParam("DrawW","Draw W", SCRIPT_PARAM_ONOFF, true)
 DrawingsConfig.addParam("DrawR","Draw R", SCRIPT_PARAM_ONOFF, true)
 Config.addParam("DMG", "DMG", SCRIPT_PARAM_ONOFF, true)
  
@@ -46,7 +45,7 @@ local trueDMG = CalcDamage(myHero, target, 0, (10*GetCastLevel(myHero,_Q) + 30 +
     end
 
 if CanUseSpell(myHero,_E) == READY then 
-local trueDMG = CalcDamage(myHero, target, 0, (20*GetCastLevel(myHero,_E) + 15 + 0.5*(GetBonusAP(myHero) + GetBonusDmg(myHero))))
+local trueDMG = CalcDamage(myHero, target, 0, (20*GetCastLevel(myHero,_E) + 15 + 0.5*(GetBonusAP(myHero) + 0.5*(GetBaseDamage(myHero) + GetBonusDmg(myHero)))))
     DrawDmgOverHpBar(target,GetCurrentHP(target),trueDMG,0,0xff0cff00)
     end
     
@@ -60,7 +59,6 @@ end)
  
 function Drawings()
 myHeroPos = GetOrigin(myHero)
-if CanUseSpell(myHero, _W) == READY and DrawingsConfig.DrawW then DrawCircle(myHeroPos.x,myHeroPos.y,myHeroPos.z,GetCastRange(myHero,_W),3,100,0xff00ff00) end
 if CanUseSpell(myHero, _Q) == READY and DrawingsConfig.DrawQ then DrawCircle(myHeroPos.x,myHeroPos.y,myHeroPos.z,GetCastRange(myHero,_Q),3,100,0xff00ff00) end
 if CanUseSpell(myHero, _E) == READY and DrawingsConfig.DrawE then DrawCircle(myHeroPos.x,myHeroPos.y,myHeroPos.z,GetCastRange(myHero,_E),3,100,0xff00ff00) end
 if CanUseSpell(myHero, _R) == READY and DrawingsConfig.DrawR then DrawCircle(myHeroPos.x,myHeroPos.y,myHeroPos.z,GetCastRange(myHero,_R),3,100,0xff00ff00) end
