ff33e683774b8cce099c56dbd9e78a55aee5417a
Zypppy <zypppaliukas@gmail.com>
2015-09-04 22:25:12 +0300

    Update Fiora.lua

--- Fiora.lua
+++ Fiora.lua
@@ -19,14 +19,14 @@ local target = GetCurrentTarget()
               local target = GetTarget(600, DAMAGE_PHYSICAL)
                 if ValidTarget(target, 600) then
                        
-					    local QPred = GetPredictionForPlayer(GetMyHeroPos(),target,GetMoveSpeed(target),0,0,400,250,false,true)
-                        if CanUseSpell(myHero, _Q) == READY and QPred.HitChance == 1 and Config.Q then
+					    local QPred = GetPredictionForPlayer(GetMyHeroPos(),target,GetMoveSpeed(target),0,0,750,250,false,true)
+                        if CanUseSpell(myHero, _Q) == READY and QPred.HitChance == 1 and ValidTarget(target, 750) and Config.Q then
                         CastSkillShot(_Q,QPred.PredPos.x,QPred.PredPos.y,QPred.PredPos.z)
                         end
-                        if CanUseSpell(myHero, _R) == READY and ValidTarget(target, GetCastRange(myHero,_R)) and IsInDistance(target, 600) and Config.R then
+                        if CanUseSpell(myHero, _R) == READY and ValidTarget(target, 500) and Config.R then
                         CastTargetSpell(target, _R)
                         end
-						if CanUseSpell(myHero, _E) == READY and IsInDistance(target, 170) and Config.E then
+						if CanUseSpell(myHero, _E) == READY and ValidTarget(target, 170) and Config.E then
                         CastSpell(_E)
 						end
                 end
@@ -35,7 +35,7 @@ local target = GetCurrentTarget()
 
         if ValidTarget(target, 2000) and Config.DMG then
   if CanUseSpell(myHero,_Q) == READY then 
-local trueDMG = CalcDamage(myHero, target, 0, (10*GetCastLevel(myHero,_Q) + 55 +  ((15*GetCastLevel(myHero,_Q) + 40) * (GetBaseDamage(myHero) + GetBonusDmg(myHero)))) / 100
+local trueDMG = CalcDamage(myHero, target, 0, (10*GetCastLevel(myHero,_Q) + 55 + (15*GetCastLevel(myHero,_Q) + (GetBaseDamage(myHero) + GetBonusDmg(myHero)))))
     DrawDmgOverHpBar(target,GetCurrentHP(target),trueDMG,0,0xff00ff00)
     end
 
@@ -49,7 +49,7 @@ end)
  
 function Drawings()
 myHeroPos = GetOrigin(myHero)
-if CanUseSpell(myHero, _Q) == READY and DrawingsConfig.DrawQ then DrawCircle(myHeroPos.x,myHeroPos.y,myHeroPos.z,GetCastRange(myHero,_Q),3,100,0xff00ff00) end
+if CanUseSpell(myHero, _Q) == READY and DrawingsConfig.DrawQ then DrawCircle(myHeroPos.x,myHeroPos.y,myHeroPos.z,750,3,100,0xff00ff00) end
 if CanUseSpell(myHero, _W) == READY and DrawingsConfig.DrawW then DrawCircle(myHeroPos.x,myHeroPos.y,myHeroPos.z,GetCastRange(myHero,_W),3,100,0xff00ff000) end
 if CanUseSpell(myHero, _E) == READY and DrawingsConfig.DrawE then DrawCircle(myHeroPos.x,myHeroPos.y,myHeroPos.z,GetCastRange(myHero,_E),3,100,0xff00ff00) end
 if CanUseSpell(myHero, _R) == READY and DrawingsConfig.DrawR then DrawCircle(myHeroPos.x,myHeroPos.y,myHeroPos.z,GetCastRange(myHero,_R),3,100,0xff00ff000) end
