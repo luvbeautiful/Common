4d2f981e4886a7d3b83f93253aaf52f1aca03a93
Zypppy <zypppaliukas@gmail.com>
2015-09-06 11:52:24 +0300

    Update Nidalee.lua

--- Nidalee.lua
+++ Nidalee.lua
@@ -1,5 +1,6 @@
 Config = scriptConfig("Nidalee", "Nidalee:")
 Config.addParam("Q", "Use Q", SCRIPT_PARAM_ONOFF, true)
+Config.addParam("E", "Use E", SCRIPT_PARAM_ONOFF, true)
 DrawingsConfig = scriptConfig("Drawings", "Drawings:")
 DrawingsConfig.addParam("DrawQ","Draw Q", SCRIPT_PARAM_ONOFF, true)
 Config.addParam("DMG", "DMG", SCRIPT_PARAM_ONOFF, true)
@@ -14,14 +15,18 @@ local target = GetCurrentTarget()
  
         if IWalkConfig.Combo then
               local target = GetTarget(1250, DAMAGE_MAGIC)
-                if ValidTarget(target, 1250) then
                        
                         local QPred = GetPredictionForPlayer(GetMyHeroPos(),target,GetMoveSpeed(target),1300,250,1500,60,true,false)
                         if CanUseSpell(myHero, _Q) == READY and QPred.HitChance == 1 and ValidTarget(target, GetCastRange(myHero,_Q)) and Config.Q then
                         CastSkillShot(_Q,QPred.PredPos.x,QPred.PredPos.y,QPred.PredPos.z)
 						end
+                     if CanUseSpell(myHero, _E) == READY and Config.E then
+                     if (GetCurrentHP(myHero)/GetMaxHP(myHero))<0.5 then
+                        CastTargetSpell(myHero, _E)
+                    end
                 end
-        end
+end
+
 
         if ValidTarget(target, 2000) and Config.DMG then
   if CanUseSpell(myHero,_Q) == READY then
