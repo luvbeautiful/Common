19bb88bcf78865fd31b86c0fd95d565b0377fafa
Zypppy <zypppaliukas@gmail.com>
2015-09-08 17:23:58 +0300

    Update Yasuo.lua

--- Yasuo.lua
+++ Yasuo.lua
@@ -1,75 +1,82 @@
-Config = scriptConfig("Yasuo", "Yasuo:")
-Config.addParam("Q", "Use Q", SCRIPT_PARAM_ONOFF, true)
-Config.addParam("E", "Use E", SCRIPT_PARAM_ONOFF, true)
-Config.addParam("R", "Use R", SCRIPT_PARAM_ONOFF, true)
-KSConfig = scriptConfig("KS", "Killsteal:")
-KSConfig.addParam("KSR", "Killsteal with R", SCRIPT_PARAM_ONOFF, true)
-KSConfig.addParam("KSQ", "Killsteal with Q", SCRIPT_PARAM_ONOFF, true)
-DrawingsConfig = scriptConfig("Drawings", "Drawings:")
-DrawingsConfig.addParam("DrawQ","Draw Q", SCRIPT_PARAM_ONOFF, true)
-Config.addParam("DMG", "DMG", SCRIPT_PARAM_ONOFF, true)
+YasuoiMenu = Menu("Yasuo", "Yasuo")
+YasuoiMenu:SubMenu("Combo", "Combo")
+YasuoiMenu.Combo:Boolean("Q", "Use Q", true)
+YasuoiMenu.Combo:Boolean("E", "Use E", true)
+YasuoiMenu.Combo:Boolean("R", "Use R", true)
+
+YasuoiMenu:SubMenu("Killsteal", "Killsteal")
+YasuoiMenu.Killsteal:Boolean("Q", "Killsteal with Q", true)
+YasuoiMenu.Killsteal:Boolean("R", "Killsteal with R", true)
+
+YasuoiMenu:SubMenu("Drawings", "Drawings")
+YasuoiMenu.Drawings:Boolean("Q", "Draw Q Range", true)
+YasuoiMenu.Drawings:Boolean("E", "Draw E Range", true)
+YasuoiMenu.Drawings:Boolean("R", "Draw R Range", true)
  
-myIAC = IAC()
  
 OnLoop(function(myHero)
-Drawings()
-Killsteal()
-local target = GetCurrentTarget()
  
-        if IWalkConfig.Combo then
-              local target = GetTarget(1000, DAMAGE_PHYSICAL)
-                if ValidTarget(target, 1000) then
+        if IOW:Mode() == "Combo" then
                        
-					    
-						local QPred = GetPredictionForPlayer(GetMyHeroPos(),target,GetMoveSpeed(target),1500,250,1025,90,false,false)
-                        if CanUseSpell(myHero, _Q) == READY and QPred.HitChance == 1 and ValidTarget(target, GetCastRange(myHero,_Q)) and Config.Q and GetDistance(myHero, enemy) < 1025 then 
+					    local target = GetCurrentTarget()
+						 local QPred = GetPredictionForPlayer(GoS:myHeroPos(),target,GetMoveSpeed(target),1500,250,1025,90,false,false)
+                        
+
+
+                        if CanUseSpell(myHero, _Q) == READY and QPred.HitChance == 1 and GoS:ValidTarget(target, GetCastRange(myHero,_Q)) and YasuoiMenu.Combo.Q:Value() then 
                         CastSkillShot(_Q,QPred.PredPos.x,QPred.PredPos.y,QPred.PredPos.z)
 						end
-						if CanUseSpell(myHero, _E) == READY and IsInDistance(target, 475) and Config.E then
+
+						if CanUseSpell(myHero, _E) == READY and GoS:ValidTarget(target, GetCastRange(myHero,_E)) and YasuoiMenu.Combo.E:Value() then
                         CastTargetSpell(target, _E)
 						end
-						if CanUseSpell(myHero, _R) == READY and IsInDistance(target, 1200) and Config.R then
+
+						if CanUseSpell(myHero, _R) == READY and GoS:ValidTarget(target, GetCastRange(myHero,_R)) and YasuoiMenu.Combo.R:Value() then
                         CastSpell(_R)
 						end
-                end
+
+
         end
 
-        if ValidTarget(target, 2000) and Config.DMG then
+       if IOW:Mode() == "DMG" then
+
+
   if CanUseSpell(myHero,_Q) == READY then
-local trueDMG = CalcDamage(myHero, target, 0, (20*GetCastLevel(myHero,_Q) + 0 + 1.0*(GetBaseDamage(myHero) + GetBonusDmg(myHero))))
+local trueDMG = GoS:CalcDamage(myHero, target, 0, (20*GetCastLevel(myHero,_Q) + 0 + 1.0*(GetBaseDamage(myHero) + GetBonusDmg(myHero))))
 DrawDmgOverHpBar(target,GetCurrentHP(target),trueDMG,0,0xff0cff00)
     end
 
     if CanUseSpell(myHero,_E) == READY then
-local trueDMG = CalcDamage(myHero, target, 0, (20*GetCastLevel(myHero,_E) + 50 + 0.6*(GetBonusAP(myHero))))
+local trueDMG = GoS:CalcDamage(myHero, target, 0, (20*GetCastLevel(myHero,_E) + 50 + 0.6*(GetBonusAP(myHero))))
     DrawDmgOverHpBar(target,GetCurrentHP(target),trueDMG,0,0xff0cff00)
     end
 
     if CanUseSpell(myHero,_R) == READY then
-local trueDMG = CalcDamage(myHero, target, 0, (100*GetCastLevel(myHero,_R) + 100 + 1.5*(GetBaseDamage(myHero) + GetBonusDmg(myHero))))
+local trueDMG = GoS:CalcDamage(myHero, target, 0, (100*GetCastLevel(myHero,_R) + 100 + 1.5*(GetBaseDamage(myHero) + GetBonusDmg(myHero))))
     DrawDmgOverHpBar(target,GetCurrentHP(target),trueDMG,0,0xff0cff00)
     end
 
+
+
  end
  
+for i,enemy in pairs(GoS:GetEnemyHeroes()) do
+
+local QPred = GetPredictionForPlayer(GoS:myHeroPos(),enemy,GetMoveSpeed(enemy),1500,250,1025,90,false,false)	          
+               
+		       if CanUseSpell(myHero, _Q) == READY and YasuoiMenu.Killsteal.Q:Value() and QPred.HitChance == 1 and GoS:ValidTarget(enemy,GetCastRange(myHero,_Q)) and GetCurrentHP(enemy) < GoS:CalcDamage(myHero, enemy, 0, (20*GetCastLevel(myHero,_Q) + 0 + 1.0*(GetBaseDamage(myHero) + GetBonusDmg(myHero)))) then 
+        CastSkillShot(_Q,QPred.PredPos.x,QPred.PredPos.y,QPred.PredPos.z)
+
+                 elseif CanUseSpell(myHero, _R) == READY and GoS:ValidTarget(enemy, GetCastRange(myHero,_R)) and YasuoiMenu.Killsteal.R:Value() and GetCurrentHP(enemy) < GoS:CalcDamage(myHero, enemy, 0, (100*GetCastLevel(myHero,_R) + 100 + 1.5*(GetBaseDamage(myHero) + GetBonusDmg(myHero)))) then
+                 CastSpell(_R)
+    end
+ 
+if YasuoiMenu.Drawings.Q:Value() then DrawCircle(GoS:myHeroPos().x, GoS:myHeroPos().y, GoS:myHeroPos().z,(GetCastRange(myHero,_Q)),3,100,0xff00ff00) end
+if YasuoiMenu.Drawings.E:Value() then DrawCircle(GoS:myHeroPos().x, GoS:myHeroPos().y, GoS:myHeroPos().z,(GetCastRange(myHero,_E)),3,100,0xff00ff00) end
+if YasuoiMenu.Drawings.R:Value() then DrawCircle(GoS:myHeroPos().x, GoS:myHeroPos().y, GoS:myHeroPos().z,(GetCastRange(myHero,_R)),3,100,0xff00ff00) end
+
+
+end
 
 end)
 
-function Killsteal()
-	      for i,enemy in pairs(GetEnemyHeroes()) do
-		       local QPred = GetPredictionForPlayer(GetMyHeroPos(),target,GetMoveSpeed(target),1500,250,1025,90,false,false)
-
-		       if CanUseSpell(myHero,_R) and ValidTarget(enemy, GetCastRange(myHero,_R)) and KSConfig.KSR and GetCurrentHP(enemy) < CalcDamage(myHero, enemy, 0, (100*GetCastLevel(myHero,_R) + 100 + 1.5*(GetBaseDamage(myHero) + GetBonusDmg(myHero)))) then
-                 CastSpell(_R)
-
-			   elseif CanUseSpell(myHero, _Q) == READY and QPred.HitChance == 1 and ValidTarget(enemy,GetCastRange(myHero,_Q)) and KSConfig.KSQ and GetCurrentHP(enemy) < CalcDamage(myHero, enemy, 0, (20*GetCastLevel(myHero,_Q) + 0 + 1.0*(GetBaseDamage(myHero) + GetBonusDmg(myHero)))) then 
-		CastSkillShot(_Q,QPred.PredPos.x,QPred.PredPos.y,QPred.PredPos.z)
-
-		end
-    end	 
-end
- 
-function Drawings()
-myHeroPos = GetOrigin(myHero)
-if CanUseSpell(myHero, _Q) == READY and DrawingsConfig.DrawQ then DrawCircle(myHeroPos.x,myHeroPos.y,myHeroPos.z,GetCastRange(myHero,_Q),3,100,0xff00ff00) end
-end
