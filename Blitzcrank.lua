1c99a88e22b3c3f0c4510810482b4b27652e1d25
Zypppy <zypppaliukas@gmail.com>
2015-09-10 13:37:58 +0300

    Update Blitzcrank.lua

--- Blitzcrank.lua
+++ Blitzcrank.lua
@@ -1,73 +1,51 @@
-Config = scriptConfig("Blitzcrank", "Blitzcrank:")
-Config.addParam("Q", "Use Q", SCRIPT_PARAM_ONOFF, true)
-Config.addParam("W", "Use W", SCRIPT_PARAM_ONOFF, true)
-Config.addParam("E", "Use E", SCRIPT_PARAM_ONOFF, true)
-Config.addParam("R", "Use R", SCRIPT_PARAM_ONOFF, true)
-KSConfig = scriptConfig("KS", "Killsteal:")
-KSConfig.addParam("KSR", "Killsteal with R", SCRIPT_PARAM_ONOFF, true)
-DrawingsConfig = scriptConfig("Drawings", "Drawings:")
-DrawingsConfig.addParam("DrawQ","Draw Q", SCRIPT_PARAM_ONOFF, true)
-DrawingsConfig.addParam("DrawE","Draw E", SCRIPT_PARAM_ONOFF, true)
-DrawingsConfig.addParam("DrawW","Draw W", SCRIPT_PARAM_ONOFF, true)
-DrawingsConfig.addParam("DrawR","Draw R", SCRIPT_PARAM_ONOFF, true)
-Config.addParam("DMG", "DMG", SCRIPT_PARAM_ONOFF, true)
+BlitzcrankiMenu = Menu("Blitzcran", "Blitzcran")
+BlitzcrankiMenu:SubMenu("Combo", "Combo")
+BlitzcrankiMenu.Combo:Boolean("Q", "Use Q", true)
+BlitzcrankiMenu.Combo:Boolean("W", "Use W", true)
+BlitzcrankiMenu.Combo:Boolean("E", "Use E", true)
+BlitzcrankiMenu.Combo:Boolean("R", "Use R", true)
 
- 
-myIAC = IAC()
- 
+BlitzcrankiMenu:SubMenu("Drawings", "Drawings")
+BlitzcrankiMenu.Drawings:Boolean("Q", "Draw Q Range", true)
+BlitzcrankiMenu.Drawings:Boolean("R", "Draw R Range", true)
+
+BlitzcrankiMenu:SubMenu("Killsteal", "Killsteal")
+BlitzcrankiMenu.Killsteal:Boolean("R", "Killsteal with R", true)
+
+  
 OnLoop(function(myHero)
-Drawings()
-Killsteal()
-local target = GetCurrentTarget() 
-        if IWalkConfig.Combo then
-              local target = GetTarget(800, DAMAGE_MAGIC)
+ 
+        if IOW:Mode() == "Combo" then
+                       
+                        local target = GetCurrentTarget()
+				local QPred = GetPredictionForPlayer(GoS:myHeroPos(),target,GetMoveSpeed(target),1800,250,1050,70,true,false)
                         
-					              local QPred = GetPredictionForPlayer(GetMyHeroPos(),target,GetMoveSpeed(target),1800,250,1050,70,true,false)
-                        if CanUseSpell(myHero, _Q) == READY and QPred.HitChance == 1 and ValidTarget(target, 1045) and Config.Q then
+                       if CanUseSpell(myHero, _Q) == READY and QPred.HitChance == 1 and GoS:ValidTarget(target, 1050) and BlitzcrankiMenu.Combo.Q:Value() then
                         CastSkillShot(_Q,QPred.PredPos.x,QPred.PredPos.y,QPred.PredPos.z)
 						            end
-                        if CanUseSpell(myHero, _W) == READY and ValidTarget(target, 600) and Config.W then
+                        if CanUseSpell(myHero, _W) == READY and GoS:ValidTarget(target, 600) and BlitzcrankiMenu.Combo.W:Value() then
                         CastSpell(_W)
                         end
-						            if CanUseSpell(myHero, _E) == READY and ValidTarget(target, 400) and Config.E then
+						            if CanUseSpell(myHero, _E) == READY and GoS:ValidTarget(target, 400) and BlitzcrankiMenu.Combo.E:Value() then
                         CastSpell(_E)
 						            end
-						            if CanUseSpell(myHero, _R) == READY and ValidTarget(target, GetCastRange(myHero,_R)) and Config.R then
+						            if CanUseSpell(myHero, _R) == READY and GoS:ValidTarget(target, GetCastRange(myHero,_R)) and BlitzcrankiMenu.Combo.R:Value() then
                         CastSpell(_R)
 						            end
         end
 
+for i,enemy in pairs(GoS:GetEnemyHeroes()) do
 
+if CanUseSpell(myHero,_R) and GoS:ValidTarget(enemy, GetCastRange(myHero,_R)) and BlitzcrankiMenu.Killsteal.R:Value() and GetCurrentHP(enemy) < GoS:CalcDamage(myHero, enemy, 0, (125*GetCastLevel(myHero,_R) + 125 + 1.0*GetBonusAP(myHero))) then
+                 CastSpell(_R)
 
-     if ValidTarget(target, 2000) and Config.DMG then
-  if CanUseSpell(myHero,_Q) == READY then 
-local trueDMG = CalcDamage(myHero, target, 0, (55*GetCastLevel(myHero,_Q) + 25 + 1.0*(GetBonusAP(myHero))))
-    DrawDmgOverHpBar(target,GetCurrentHP(target),trueDMG,0,0xff00ff00)
-    end
-
-    if CanUseSpell(myHero,_R) == READY then 
-local trueDMG = CalcDamage(myHero, target, 0, (125*GetCastLevel(myHero,_R) + 125 + 1.0*(GetBonusAP(myHero))))
-    DrawDmgOverHpBar(target,GetCurrentHP(target),trueDMG,0,0xff0cff00)
-    end
 end
 
+ 
+if BlitzcrankiMenu.Drawings.Q:Value() then DrawCircle(GoS:myHeroPos().x, GoS:myHeroPos().y, GoS:myHeroPos().z,(GetCastRange(myHero,_Q)),3,100,0xff00ff00) end
+if BlitzcrankiMenu.Drawings.R:Value() then DrawCircle(GoS:myHeroPos().x, GoS:myHeroPos().y, GoS:myHeroPos().z,(GetCastRange(myHero,_R)),3,100,0xff00ff00) end
 
+
+end
 
 end)
-
-
- function Killsteal()
-	      for i,enemy in pairs(GetEnemyHeroes()) do
-		       if CanUseSpell(myHero,_R) and ValidTarget(enemy, GetCastRange(myHero,_R)) and KSConfig.KSR and GetCurrentHP(enemy) < CalcDamage(myHero, enemy, 0, (125*GetCastLevel(myHero,_R) + 125 + 1.0*GetBonusAP(myHero))) then
-                 CastSpell(_R)
-			   end
-		end
-end	 
- 
-function Drawings()
-myHeroPos = GetOrigin(myHero)
-if CanUseSpell(myHero, _W) == READY and DrawingsConfig.DrawW then DrawCircle(myHeroPos.x,myHeroPos.y,myHeroPos.z,600,3,100,0xff00ff00) end
-if CanUseSpell(myHero, _Q) == READY and DrawingsConfig.DrawQ then DrawCircle(myHeroPos.x,myHeroPos.y,myHeroPos.z,GetCastRange(myHero,_Q),3,100,0xff00ff00) end
-if CanUseSpell(myHero, _E) == READY and DrawingsConfig.DrawE then DrawCircle(myHeroPos.x,myHeroPos.y,myHeroPos.z,GetCastRange(myHero,_E),3,100,0xff00ff00) end
-if CanUseSpell(myHero, _R) == READY and DrawingsConfig.DrawR then DrawCircle(myHeroPos.x,myHeroPos.y,myHeroPos.z,GetCastRange(myHero,_R),3,100,0xff00ff00) end
-end
