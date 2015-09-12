26aaaf84b8620356f52e1f4c225471ad97a42f25
Zypppy <zypppaliukas@gmail.com>
2015-09-12 23:54:40 +0300

    Update Evelynn.lua

--- Evelynn.lua
+++ Evelynn.lua
@@ -1,63 +1,45 @@
-Config = scriptConfig("Evelynn", "Evelynn:")
-Config.addParam("Q", "Use Q", SCRIPT_PARAM_ONOFF, true)
-Config.addParam("W", "Use W", SCRIPT_PARAM_ONOFF, true)
-Config.addParam("E", "Use E", SCRIPT_PARAM_ONOFF, true)
-Config.addParam("R", "Use R", SCRIPT_PARAM_ONOFF, true)
-DrawingsConfig = scriptConfig("Drawings", "Drawings:")
-DrawingsConfig.addParam("DrawQ","Draw Q", SCRIPT_PARAM_ONOFF, true)
-DrawingsConfig.addParam("DrawE","Draw E", SCRIPT_PARAM_ONOFF, true)
-DrawingsConfig.addParam("DrawR","Draw R", SCRIPT_PARAM_ONOFF, true)
-Config.addParam("DMG", "DMG", SCRIPT_PARAM_ONOFF, true)
- 
-myIAC = IAC()
+EvelynnMenu = Menu("Evelynn", "Evelynn")
+EvelynnMenu:SubMenu("Combo", "Combo")
+EvelynnMenu.Combo:Boolean("Q", "Use Q", true)
+EvelynnMenu.Combo:Boolean("W", "Use W", true)
+EvelynnMenu.Combo:Boolean("E", "Use E", true)
+EvelynnMenu.Combo:Boolean("R", "Use R", true)
+
+EvelynnMenu:SubMenu("Drawings", "Drawings:")
+EvelynnMenu.Drawings:Boolean("E","Draw E", true)
+EvelynnMenu.Drawings:Boolean("W","Draw W", true)
+EvelynnMenu.Drawings:Boolean("R","Draw R", true)
+EvelynnMenu.Drawings:Boolean("Q","Draw Q", true)
  
 OnLoop(function(myHero)
-Drawings()
+
 local target = GetCurrentTarget()
- 
-        if IWalkConfig.Combo then
-              local target = GetTarget(1150, DAMAGE_MAGIC)
+
+if IOW:Mode() == "Combo" then
+                       local RPred = GetPredictionForPlayer(GoS:myHeroPos(),target,GetMoveSpeed(target),0,250,650,350,false,false)
                        
-					    if CanUseSpell(myHero, _Q) == READY and ValidTarget(target, 500) and Config.Q then
+					    if CanUseSpell(myHero, _Q) == READY and GoS:ValidTarget(target, 500) and EvelynnMenu.Combo.Q:Value() then
                         CastSpell(_Q)
 						end
-                        if CanUseSpell(myHero, _W) == READY and ValidTarget(target, 600) and GetDistance(myHero, target) < 600 and Config.W then
+                        if CanUseSpell(myHero, _W) == READY and GoS:ValidTarget(target, 600) and GoS:GetDistance(myHero, target) < 700 and EvelynnMenu.Combo.W:Value() then
                         CastSpell(_W)
                         end
-						if CanUseSpell(myHero, _E) == READY and ValidTarget(target, GetCastRange(myHero,_E)) and Config.E then
+						if CanUseSpell(myHero, _E) == READY and GoS:ValidTarget(target, 300) and EvelynnMenu.Combo.E:Value() then
                         CastTargetSpell(target, _E)
 						end
-						local RPred = GetPredictionForPlayer(GetMyHeroPos(),target,GetMoveSpeed(target),0,250,650,350,false,false)
-                        if CanUseSpell(myHero, _R) == READY and RPred.HitChance == 1 and ValidTarget(target, GetCastRange(myHero,_R)) and Config.R then
+                        if CanUseSpell(myHero, _R) == READY and RPred.HitChance == 1 and GoS:ValidTarget(target, 625) and EvelynnMenu.Combo.R:Value() then
                             if (GetCurrentHP(myHero)/GetMaxHP(myHero))<0.4 then
                         CastSkillShot(_R,RPred.PredPos.x,RPred.PredPos.y,RPred.PredPos.z)
 						end
                 end
         end
 
-
-        if ValidTarget(target, 2000) and Config.DMG then
-  if CanUseSpell(myHero,_Q) == READY then 
-local trueDMG = CalcDamage(myHero, target, 0, (10*GetCastLevel(myHero,_Q) + 30 + 0.55*(GetBonusAP(myHero) + GetBonusDmg(myHero))))
-    DrawDmgOverHpBar(target,GetCurrentHP(target),trueDMG,0,0xff00ff00)
-    end
-
-if CanUseSpell(myHero,_E) == READY then 
-local trueDMG = CalcDamage(myHero, target, 0, (20*GetCastLevel(myHero,_E) + 15 + 0.5*(GetBonusAP(myHero) + 0.5*(GetBaseDamage(myHero) + GetBonusDmg(myHero)))))
-    DrawDmgOverHpBar(target,GetCurrentHP(target),trueDMG,0,0xff0cff00)
-    end
-    
-end
+if EvelynnMenu.Drawings.Q:Value() then DrawCircle(GoS:myHeroPos().x, GoS:myHeroPos().y, GoS:myHeroPos().z,500,3,100,0xff00ff00) end
+if EvelynnMenu.Drawings.W:Value() then DrawCircle(GoS:myHeroPos().x, GoS:myHeroPos().y, GoS:myHeroPos().z,600,3,100,0xff00ff00) end
+if EvelynnMenu.Drawings.E:Value() then DrawCircle(GoS:myHeroPos().x, GoS:myHeroPos().y, GoS:myHeroPos().z,300,3,100,0xff00ff00) end
+if EvelynnMenu.Drawings.R:Value() then DrawCircle(GoS:myHeroPos().x, GoS:myHeroPos().y, GoS:myHeroPos().z,625,3,100,0xff00ff00) end
 
 
+    end)
  
-end)
 
-
- 
-function Drawings()
-myHeroPos = GetOrigin(myHero)
-if CanUseSpell(myHero, _Q) == READY and DrawingsConfig.DrawQ then DrawCircle(myHeroPos.x,myHeroPos.y,myHeroPos.z,GetCastRange(myHero,_Q),3,100,0xff00ff00) end
-if CanUseSpell(myHero, _E) == READY and DrawingsConfig.DrawE then DrawCircle(myHeroPos.x,myHeroPos.y,myHeroPos.z,GetCastRange(myHero,_E),3,100,0xff00ff00) end
-if CanUseSpell(myHero, _R) == READY and DrawingsConfig.DrawR then DrawCircle(myHeroPos.x,myHeroPos.y,myHeroPos.z,GetCastRange(myHero,_R),3,100,0xff00ff00) end
-end
