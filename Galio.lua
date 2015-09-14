9237c83fe544070f3e4a18ed69d91e72f6d04e7c
Zypppy <zypppaliukas@gmail.com>
2015-09-14 16:54:02 +0300

    Update Galio.lua

--- Galio.lua
+++ Galio.lua
@@ -1,57 +1,47 @@
-Config = scriptConfig("Galio", "Galio:")
-Config.addParam("Q", "Use Q", SCRIPT_PARAM_ONOFF, true)
-Config.addParam("W", "Use W", SCRIPT_PARAM_ONOFF, true)
-Config.addParam("E", "Use E", SCRIPT_PARAM_ONOFF, true)
-DrawingsConfig = scriptConfig("Drawings", "Drawings:")
-DrawingsConfig.addParam("DrawQ","Draw Q", SCRIPT_PARAM_ONOFF, true)
-DrawingsConfig.addParam("DrawW","Draw W", SCRIPT_PARAM_ONOFF, true)
-DrawingsConfig.addParam("DrawE","Draw E", SCRIPT_PARAM_ONOFF, true)
-Config.addParam("DMG", "DMG", SCRIPT_PARAM_ONOFF, true)
- 
- 
-myIAC = IAC()
+GalioMenu = Menu("Galio", "Galio")
+GalioMenu:SubMenu("Combo", "Combo")
+GalioMenu.Combo:Boolean("Q", "Use Q", true)
+GalioMenu.Combo:Boolean("W", "Use W", true)
+GalioMenu.Combo:Boolean("E", "Use E", true)
+
+GalioMenu:SubMenu("Drawings", "Drawings:")
+GalioMenu.Drawings:Boolean("Q","Draw Q", true)
+GalioMenu.Drawings:Boolean("W","Draw W", true)
+GalioMenu.Drawings:Boolean("E","Draw E", true)
  
 OnLoop(function(myHero)
-Drawings()
+
+
 local target = GetCurrentTarget()
- 
-        if IWalkConfig.Combo then
-              local target = GetTarget(1200, DAMAGE_MAGIC)
-                       
-                        local QPred = GetPredictionForPlayer(GetMyHeroPos(),target,GetMoveSpeed(target),1300,300,900,200,false,true)
-                        if CanUseSpell(myHero, _Q) == READY and QPred.HitChance == 1 and ValidTarget(target, GetCastRange(myHero,_Q)) and Config.Q then
+
+
+
+if IOW:Mode() == "Combo" then
+                       local QPred = GetPredictionForPlayer(GoS:myHeroPos(),target,GetMoveSpeed(target),1300,300,900,200,false,false)
+                        local EPred = GetPredictionForPlayer(GoS:myHeroPos(),target,GetMoveSpeed(target),1200,300,1200,120,false,false)
+
+
+
+                        if CanUseSpell(myHero, _Q) == READY and QPred.HitChance == 1 and GoS:ValidTarget(target, GetCastRange(myHero,_Q)) and GalioMenu.Combo.Q:Value() then
                         CastSkillShot(_Q,QPred.PredPos.x,QPred.PredPos.y,QPred.PredPos.z)
                         end
-                        if CanUseSpell(myHero, _W) == READY and ValidTarget(target, 500) and IsInDistance(target, 500) and Config.W then
-                        CastSpell(_W)
+                         for _, ally in pairs(GoS:GetAllyHeroes()) do
+                      if  CanUseSpell(myHero, _W) == READY and GoS:IsInDistance(ally, (GetCastRange(myHero,_W))) and (GetCurrentHP(ally)/GetMaxHP(ally))<0.7 and GalioMenu.Combo.W:Value() then
+                        CastTargetSpell(ally ,_W)
+                    elseif  CanUseSpell(myHero, _W) == READY and GoS:IsInDistance(myHero, (GetCastRange(myHero,_W))) and (GetCurrentHP(myHero)/GetMaxHP(myHero))<0.7 then
+                        CastTargetSpell(myHero ,_W)
 						end
-						local EPred = GetPredictionForPlayer(GetMyHeroPos(),target,GetMoveSpeed(target),1200,300,1200,120,false,true)
-                        if CanUseSpell(myHero, _E) == READY and EPred.HitChance == 1 and ValidTarget(target, GetCastRange(myHero,_E)) and Config.E then
+						end
+                        if CanUseSpell(myHero, _E) == READY and EPred.HitChance == 1 and GoS:ValidTarget(target, GetCastRange(myHero,_E)) and GalioMenu.Combo.E:Value() then
                         CastSkillShot(_E,EPred.PredPos.x,EPred.PredPos.y,EPred.PredPos.z)
 						end
         end
 
 
-        if ValidTarget(target, 2000) and Config.DMG then
-  if CanUseSpell(myHero,_Q) == READY then 
-local trueDMG = CalcDamage(myHero, target, 0, (55*GetCastLevel(myHero,_Q) + 25 + 0.6*(GetBonusAP(myHero))))
-    DrawDmgOverHpBar(target,GetCurrentHP(target),trueDMG,0,0xff00ff00)
-    end
 
-    if CanUseSpell(myHero,_E) == READY then 
-local trueDMG = CalcDamage(myHero, target, 0, (45*GetCastLevel(myHero,_E) + 15 + 0.5*(GetBonusAP(myHero))))
-    DrawDmgOverHpBar(target,GetCurrentHP(target),trueDMG,0,0xff0cff00)
-    end
-    
-end
+if GalioMenu.Drawings.Q:Value() then DrawCircle(GoS:myHeroPos().x, GoS:myHeroPos().y, GoS:myHeroPos().z,(GetCastRange(myHero,_Q)),3,100,0xff00ff00) end
+if GalioMenu.Drawings.W:Value() then DrawCircle(GoS:myHeroPos().x, GoS:myHeroPos().y, GoS:myHeroPos().z,(GetCastRange(myHero,_W)),3,100,0xff00ff00) end
+if GalioMenu.Drawings.E:Value() then DrawCircle(GoS:myHeroPos().x, GoS:myHeroPos().y, GoS:myHeroPos().z,(GetCastRange(myHero,_E)),3,100,0xff00ff00) end
 
 
- 
-end)
- 
-function Drawings()
-myHeroPos = GetOrigin(myHero)
-if CanUseSpell(myHero, _Q) == READY and DrawingsConfig.DrawQ then DrawCircle(myHeroPos.x,myHeroPos.y,myHeroPos.z,GetCastRange(myHero,_Q),3,100,0xff00ff00) end
-if CanUseSpell(myHero, _W) == READY and DrawingsConfig.DrawW then DrawCircle(myHeroPos.x,myHeroPos.y,myHeroPos.z,GetCastRange(myHero,_W),3,100,0xff00fc000) end
-if CanUseSpell(myHero, _E) == READY and DrawingsConfig.DrawE then DrawCircle(myHeroPos.x,myHeroPos.y,myHeroPos.z,GetCastRange(myHero,_E),3,100,0xff00ffc0) end
-end
+    end)
