354024bad2008c82a02dbe7213bd1b08a8bdb91c
Zypppy <zypppaliukas@gmail.com>
2015-09-15 12:15:14 +0300

    Update Gragas.lua

--- Gragas.lua
+++ Gragas.lua
@@ -1,83 +1,67 @@
-Config = scriptConfig("Gragas", "Gragas:")
-Config.addParam("Q", "Use Q", SCRIPT_PARAM_ONOFF, true)
-Config.addParam("W", "Use W", SCRIPT_PARAM_ONOFF, true)
-Config.addParam("E", "Use E", SCRIPT_PARAM_ONOFF, true)
-Config.addParam("R", "Use R", SCRIPT_PARAM_ONOFF, true)
-KSConfig = scriptConfig("KS", "Killsteal:")
-KSConfig.addParam("KSR", "Killsteal with R", SCRIPT_PARAM_ONOFF, false)
-DrawingsConfig = scriptConfig("Drawings", "Drawings:")
-DrawingsConfig.addParam("DrawQ","Draw Q", SCRIPT_PARAM_ONOFF, true)
-DrawingsConfig.addParam("DrawE","Draw E", SCRIPT_PARAM_ONOFF, true)
-DrawingsConfig.addParam("DrawR","Draw R", SCRIPT_PARAM_ONOFF, true)
-Config.addParam("DMG", "DMG", SCRIPT_PARAM_ONOFF, true)
- 
- 
-myIAC = IAC()
+GragasMenu = Menu("Gragas", "Gragas")
+GragasMenu:SubMenu("Combo", "Combo")
+GragasMenu.Combo:Boolean("Q", "Use Q", true)
+GragasMenu.Combo:Boolean("W", "Use W", true)
+GragasMenu.Combo:Boolean("E", "Use E", true)
+GragasMenu.Combo:Boolean("R", "Use R", true)
+
+GragasMenu:SubMenu("Killsteal", "Killsteal:")
+GragasMenu.Killsteal:Boolean("R", "Killsteal with R", true)
+
+
+GragasMenu:SubMenu("Drawings", "Drawings:")
+GragasMenu.Drawings:Boolean("Q","Draw Q", true)
+GragasMenu.Drawings:Boolean("W","Draw W", true)
+GragasMenu.Drawings:Boolean("E","Draw E", true)
+GragasMenu.Drawings:Boolean("R","Draw R", true)
  
 OnLoop(function(myHero)
-Drawings()
-Killsteal()
+
+
 local target = GetCurrentTarget()
- 
-        if IWalkConfig.Combo then
-              local target = GetTarget(1300, DAMAGE_MAGIC)
-                       
-                        local QPred = GetPredictionForPlayer(GetMyHeroPos(),target,GetMoveSpeed(target),1000,300,1100,275,false,true)
-                        if CanUseSpell(myHero, _Q) == READY and QPred.HitChance == 1 and ValidTarget(target, GetCastRange(myHero,_Q)) and Config.Q then
+
+
+
+if IOW:Mode() == "Combo" then
+                     local QPred = GetPredictionForPlayer(GoS:myHeroPos(),target,GetMoveSpeed(target),1000,300,1100,275,false,true)
+                      local EPred = GetPredictionForPlayer(GoS:myHeroPos(),target,GetMoveSpeed(target),1200,0,950,200,true,true)
+                       local RPred = GetPredictionForPlayer(GoS:myHeroPos(),target,GetMoveSpeed(target),1750,250,1050,350,false,true)
+
+
+
+
+
+                        if CanUseSpell(myHero, _Q) == READY and QPred.HitChance == 1 and GoS:ValidTarget(target, GetCastRange(myHero,_Q)) and GragasMenu.Combo.Q:Value() then
                         CastSkillShot(_Q,QPred.PredPos.x,QPred.PredPos.y,QPred.PredPos.z)
                         end
-						if CanUseSpell(myHero, _W) == READY and ValidTarget(target, 300) and GetDistance(myHero, target) < 300 and Config.W then
+						if CanUseSpell(myHero, _W) == READY and GoS:ValidTarget(target, 300) and GragasMenu.Combo.W:Value() then
                         CastSpell(_W)
 						end
-						local EPred = GetPredictionForPlayer(GetMyHeroPos(),target,GetMoveSpeed(target),1200,0,950,200,true,true)
-                        if CanUseSpell(myHero, _E) == READY and EPred.HitChance == 1 and ValidTarget(target, GetCastRange(myHero,_E)) and Config.E then
+                        if CanUseSpell(myHero, _E) == READY and EPred.HitChance == 1 and GoS:ValidTarget(target, GetCastRange(myHero,_E)) and GragasMenu.Combo.E:Value() then
                         CastSkillShot(_E,EPred.PredPos.x,EPred.PredPos.y,EPred.PredPos.z)
 						end
-						local RPred = GetPredictionForPlayer(GetMyHeroPos(),target,GetMoveSpeed(target),1750,250,1050,350,false,true)
-                        if CanUseSpell(myHero, _R) == READY and QPred.HitChance == 1 and ValidTarget(target, GetCastRange(myHero,_R)) and Config.R then
+						
+                        if CanUseSpell(myHero, _R) == READY and QPred.HitChance == 1 and GoS:ValidTarget(target, 1050) and GragasMenu.Combo.R:Value() then
                         CastSkillShot(_R,RPred.PredPos.x,RPred.PredPos.y,RPred.PredPos.z)
 						end
         end
 
-        if ValidTarget(target, 2000) and Config.DMG then
-  if CanUseSpell(myHero,_Q) == READY then
-local trueDMG = CalcDamage(myHero, target, 0, (28*GetCastLevel(myHero,_Q) + 28 + 0.42*(GetBonusAP(myHero))))
-    DrawDmgOverHpBar(target,GetCurrentHP(target),trueDMG,0,0xff0cff00)
-    end
+        for i,enemy in pairs(GoS:GetEnemyHeroes()) do
 
-    if CanUseSpell(myHero,_W) == READY then 
-local trueDMG = CalcDamage(myHero, target, 0, (30*GetCastLevel(myHero,_W) - 10 + 0.3*(GetBonusAP(myHero))))
-    DrawDmgOverHpBar(target,GetCurrentHP(target),trueDMG,0,0xff0cff00)
-    end
+local RPred = GetPredictionForPlayer(GoS:myHeroPos(),enemy,GetMoveSpeed(enemy),1750,250,1050,350,false,true)
 
-    if CanUseSpell(myHero,_E) == READY then 
-local trueDMG = CalcDamage(myHero, target, 0, (50*GetCastLevel(myHero,_E) + 30 + 0.6*(GetBonusAP(myHero))))
-    DrawDmgOverHpBar(target,GetCurrentHP(target),trueDMG,0,0xff0cff00)
-  end
-
-    if CanUseSpell(myHero,_R) == READY then 
-local trueDMG = CalcDamage(myHero, target, 0, (100*GetCastLevel(myHero,_R) + 100 + 0.7*(GetBonusAP(myHero))))
-    DrawDmgOverHpBar(target,GetCurrentHP(target),trueDMG,0,0xff0cff00)
-    end
-    
-end
-
-
-     
-end)
-
-function Killsteal()
-        for i,enemy in pairs(GetEnemyHeroes()) do
-	           local RPred = GetPredictionForPlayer(GetMyHeroPos(),enemy,GetMoveSpeed(enemy),1750,250,1050,350,false,true)
-  	           if CanUseSpell(myHero, _R) == READY and RPred.HitChance == 1 and ValidTarget(enemy,GetCastRange(myHero,_R)) and KSConfig.KSR and GetCurrentHP(enemy) < CalcDamage(myHero,  enemy, 0, (100*GetCastLevel(myHero,_R) + 100 + 0.7*(GetBonusAP(myHero)))) then 
+	           
+  	           if CanUseSpell(myHero, _R) == READY and RPred.HitChance == 1 and GoS:ValidTarget(enemy,GetCastRange(myHero,_R)) and GragasMenu.Killsteal.R:Value() and GetCurrentHP(enemy) < GoS:CalcDamage(myHero,  enemy, 0, (100*GetCastLevel(myHero,_R) + 100 + 0.7*(GetBonusAP(myHero)))) then 
               CastSkillShot(_R,RPred.PredPos.x,RPred.PredPos.y,RPred.PredPos.z)
              end
       end
-end
- 
-function Drawings()
-myHeroPos = GetOrigin(myHero)
-if CanUseSpell(myHero, _Q) == READY and DrawingsConfig.DrawQ then DrawCircle(myHeroPos.x,myHeroPos.y,myHeroPos.z,GetCastRange(myHero,_Q),3,100,0xff00ff00) end
-if CanUseSpell(myHero, _E) == READY and DrawingsConfig.DrawE then DrawCircle(myHeroPos.x,myHeroPos.y,myHeroPos.z,GetCastRange(myHero,_E),3,100,0xff00ffc0) end
-if CanUseSpell(myHero, _R) == READY and DrawingsConfig.DrawR then DrawCircle(myHeroPos.x,myHeroPos.y,myHeroPos.z,GetCastRange(myHero,_R),3,100,0xff00ffd0) end
-end
+
+
+
+if GragasMenu.Drawings.Q:Value() then DrawCircle(GoS:myHeroPos().x, GoS:myHeroPos().y, GoS:myHeroPos().z,(GetCastRange(myHero,_Q)),3,100,0xff00ff00) end
+if GragasMenu.Drawings.W:Value() then DrawCircle(GoS:myHeroPos().x, GoS:myHeroPos().y, GoS:myHeroPos().z,300,3,100,0xff00ff00) end
+if GragasMenu.Drawings.E:Value() then DrawCircle(GoS:myHeroPos().x, GoS:myHeroPos().y, GoS:myHeroPos().z,(GetCastRange(myHero,_E)),3,100,0xff00ff00) end
+if GragasMenu.Drawings.R:Value() then DrawCircle(GoS:myHeroPos().x, GoS:myHeroPos().y, GoS:myHeroPos().z,1050,3,100,0xff00ff00) end
+
+
+    end)
