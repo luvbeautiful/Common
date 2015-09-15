0aa1091644433950c6c62f6ea6e9a4f62f0c52fd
Zypppy <zypppaliukas@gmail.com>
2015-09-15 20:04:00 +0300

    Update Orianna.lua

--- Orianna.lua
+++ Orianna.lua
@@ -1,37 +1,24 @@
-Config = scriptConfig("Orianna", "Orianna:")
-Config.addParam("Q", "Use Q", SCRIPT_PARAM_ONOFF, true)
-DrawingsConfig = scriptConfig("Drawings", "Drawings:")
-DrawingsConfig.addParam("DrawQ","Draw Q", SCRIPT_PARAM_ONOFF, true)
-Config.addParam("DMG", "DMG", SCRIPT_PARAM_ONOFF, true)
+OriannaMenu = Menu("Orianna", "Orianna")
+OriannaMenu:SubMenu("Combo", "Combo")
+OriannaMenu.Combo:Boolean("Q", "Use Q", true)
+
+OriannaMenu:SubMenu("Drawings", "Drawings:")
+OriannaMenu.Drawings:Boolean("Q","Draw Q", true)
  
- 
-myIAC = IAC()
  
 OnLoop(function(myHero)
-Drawings()
+
 local target = GetCurrentTarget()
- 
-        if IWalkConfig.Combo then
-              local target = GetTarget(2000, DAMAGE_MAGIC)
+local mousePos = GetMousePos()
+
+if IOW:Mode() == "Combo" then
+              local QPred = GetPredictionForPlayer(GoS:myHeroPos(),target,GetMoveSpeed(target),1200,0,2000,80,false,true)
                        
-                        local QPred = GetPredictionForPlayer(GetMyHeroPos(),target,GetMoveSpeed(target),1200,0,2000,80,false,true)
-                        if CanUseSpell(myHero, _Q) == READY and QPred.HitChance == 1 and ValidTarget(target, 2000) and Config.Q then
+                        if CanUseSpell(myHero, _Q) == READY and QPred.HitChance == 1 and GoS:ValidTarget(target, 2000) and OriannaMenu.Combo.Q:Value() then
                         CastSkillShot(_Q,QPred.PredPos.x,QPred.PredPos.y,QPred.PredPos.z)
                         end
 			
-        end
+        end	
+if OriannaMenu.Drawings.Q:Value() then DrawCircle(GoS:myHeroPos().x, GoS:myHeroPos().y, GoS:myHeroPos().z,(GetCastRange(myHero,_Q)),3,100,0xff00ff00) end
 
-        if ValidTarget(target, 2000) and Config.DMG then
-  if CanUseSpell(myHero,_Q) == READY then
-local trueDMG = CalcDamage(myHero, target, 0, (30*GetCastLevel(myHero,_Q) + 30 + 0.5*(GetBonusAP(myHero))))
-    DrawDmgOverHpBar(target,GetCurrentHP(target),trueDMG,0,0xff0cff00)
-    end
-
-end
-
-end)		
- 
-function Drawings()
-myHeroPos = GetOrigin(myHero)
-if CanUseSpell(myHero, _Q) == READY and DrawingsConfig.DrawQ then DrawCircle(myHeroPos.x,myHeroPos.y,myHeroPos.z,GetCastRange(myHero,_Q),3,100,0xff00ff00) end
-end
+end)
