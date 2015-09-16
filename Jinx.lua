e3e20509e44ca02ed13e860e864eae34f8d08d5a
Zypppy <zypppaliukas@gmail.com>
2015-09-16 21:47:23 +0300

    Update Jinx.lua

--- Jinx.lua
+++ Jinx.lua
@@ -1,79 +1,68 @@
-Config = scriptConfig("Jinx", "Jinx:")
-Config.addParam("Q", "Use Q", SCRIPT_PARAM_ONOFF, true)
-Config.addParam("W", "Use W", SCRIPT_PARAM_ONOFF, true)
-Config.addParam("E", "Use E ", SCRIPT_PARAM_ONOFF, true)
-Config.addParam("R", "Use R", SCRIPT_PARAM_ONOFF, true)
-KSConfig = scriptConfig("KS", "Killsteal:")
-KSConfig.addParam("KSR", "Killsteal with R", SCRIPT_PARAM_ONOFF, true)
-DrawingsConfig = scriptConfig("Drawings", "Drawings:")
-DrawingsConfig.addParam("DrawQ","Draw Q", SCRIPT_PARAM_ONOFF, true)
-DrawingsConfig.addParam("DrawE","Draw E", SCRIPT_PARAM_ONOFF, true)
-DrawingsConfig.addParam("DrawW","Draw W", SCRIPT_PARAM_ONOFF, true)
-DrawingsConfig.addParam("DrawR","Draw R", SCRIPT_PARAM_ONOFF, true)
-Config.addParam("DMG", "DMG", SCRIPT_PARAM_ONOFF, true)
- 
-myIAC = IAC()
+JinxMenu = Menu("Jinx", "Jinx")
+JinxMenu:SubMenu("Combo", "Combo")
+JinxMenu.Combo:Boolean("Q", "Use Q", true)
+JinxMenu.Combo:Boolean("W", "Use W", true)
+JinxMenu.Combo:Boolean("E", "Use E", true)
+JinxMenu.Combo:Boolean("R", "Use R", true)
+
+JinxMenu:SubMenu("Killsteal", "Killsteal:")
+JinxMenu.Killsteal:Boolean("R", "Killsteal with R", true)
+
+JinxMenu:SubMenu("Drawings", "Drawings:")
+JinxMenu.Drawings:Boolean("E","Draw E", true)
+JinxMenu.Drawings:Boolean("R","Draw R", true)
+JinxMenu.Drawings:Boolean("Q","Draw Q", true)
+JinxMenu.Drawings:Boolean("W","Draw W", true)
  
 OnLoop(function(myHero)
-Drawings()
-Killsteal()
-local target = GetCurrentTarget()
+
+
+
+    local target = GetCurrentTarget()
  
-        if IWalkConfig.Combo then
-              local target = GetTarget(1600, DAMAGE_PHYSICAL)
-			  if ValidTarget(target, 1150) then
+        if IOW:Mode() == "Combo" then
+                       
+                        local WPred = GetPredictionForPlayer(GoS:myHeroPos(),target,GetMoveSpeed(target),3300,600,1500,60,true,false)
+                        local EPred = GetPredictionForPlayer(GoS:myHeroPos(),target,GetMoveSpeed(target),1000,250,900,50,false,false)
+                        local RPred = GetPredictionForPlayer(GoS:myHeroPos(),target,GetMoveSpeed(target),1700,600,2000,140,false,true)
+
+
+
 			  
-					    if CanUseSpell(myHero, _Q) == READY and Config.Q and ValidTarget(target, 700) then
-                        if GetDistance(myHero, target) > 525 and GotBuff(myHero, "jinxqicon") > 0 then
+					    if CanUseSpell(myHero, _Q) == READY and JinxMenu.Combo.Q:Value() and GoS:ValidTarget(target, 700) then
+                        if GoS:GetDistance(myHero, target) > 525 and GotBuff(myHero, "jinxqicon") > 0 then
                         CastSpell(_Q)
-                        elseif GetDistance(myHero, target) < 570 and GotBuff(myHero, "JinxQ") > 0 then
+                        elseif GoS:GetDistance(myHero, target) < 570 and GotBuff(myHero, "JinxQ") > 0 then
                         CastSpell(_Q)
 						end
 						end
-                        local WPred = GetPredictionForPlayer(GetMyHeroPos(),target,GetMoveSpeed(target),3300,600,1500,60,true,false)
-                        if CanUseSpell(myHero, _W) == READY and WPred.HitChance == 1 and ValidTarget(target, GetCastRange(myHero,_W)) and Config.W then
+                        if CanUseSpell(myHero, _W) == READY and WPred.HitChance == 1 and GoS:ValidTarget(target, GetCastRange(myHero,_W)) and JinxMenu.Combo.W:Value() then
                         CastSkillShot(_W,WPred.PredPos.x,WPred.PredPos.y,WPred.PredPos.z)
                         end
-						local EPred = GetPredictionForPlayer(GetMyHeroPos(),target,GetMoveSpeed(target),0,300,900,50,false,true)
-                        if CanUseSpell(myHero, _E) == READY and EPred.HitChance == 1 and ValidTarget(target, GetCastRange(myHero,_E)) and Config.E then
+                        if CanUseSpell(myHero, _E) == READY and EPred.HitChance == 1 and GoS:ValidTarget(target, GetCastRange(myHero,_E)) and JinxMenu.Combo.E:Value() then
                         CastSkillShot(_E,EPred.PredPos.x,EPred.PredPos.y,EPred.PredPos.z)
 						end
-						local RPred = GetPredictionForPlayer(GetMyHeroPos(),target,GetMoveSpeed(target),1700,600,2000,140,false,true)
-                        if CanUseSpell(myHero, _R) == READY and RPred.HitChance == 1 and ValidTarget(target, GetCastRange(myHero,_R)) and Config.R then
+                        if CanUseSpell(myHero, _R) == READY and RPred.HitChance == 1 and GoS:ValidTarget(target, GetCastRange(myHero,_R)) and JinxMenu.Combo.R:Value() then
                         CastSkillShot(_R,RPred.PredPos.x,RPred.PredPos.y,RPred.PredPos.z)
 						end
                 end
-        end
 
-        if ValidTarget(target, 2000) and Config.DMG then
-    if CanUseSpell(myHero,_W) == READY then
-local trueDMG = CalcDamage(myHero, target, 0, (50*GetCastLevel(myHero,_W) + 40 + 1.4*(GetBaseDamage(myHero) + GetBonusDmg(myHero))))
-    DrawDmgOverHpBar(target,GetCurrentHP(target),trueDMG,0,0xff0cff00)
-    end
 
-    if CanUseSpell(myHero,_E) == READY then
-local trueDMG = CalcDamage(myHero, target, 0, (55*GetCastLevel(myHero,_E) + 25 + 1.0*(GetBonusAP(myHero))))
-    DrawDmgOverHpBar(target,GetCurrentHP(target),trueDMG,0,0xff0cff00)
-    end
 
- end
- 
+          for i,enemy in pairs(GoS:GetEnemyHeroes()) do
+               local RPred = GetPredictionForPlayer(GoS:myHeroPos(),enemy,GetMoveSpeed(enemy),1700,600,2000,140,false,true)
 
-end)
 
-function Killsteal()
-	      for i,enemy in pairs(GetEnemyHeroes()) do
-		       local RPred = GetPredictionForPlayer(GetMyHeroPos(),enemy,GetMoveSpeed(enemy),1700,600,2000,140,false,true)
-               if CanUseSpell(myHero, _R) and RPred.HitChance == 1 and ValidTarget(enemy, GetCastRange(myHero, _R)) and KSConfig.KSR and RPred.HitChance == 1 and GetCurrentHP(enemy) < CalcDamage(myHero, enemy, (GetMaxHP(enemy)-GetCurrentHP(enemy))*(0.2+0.05*GetCastLevel(myHero, _R))+(150+100*GetCastLevel(myHero, _R)+GetBonusDmg(myHero))*math.max(0.1, math.min(1, GetDistance(enemy)/1700))) and GetDistance(myHero, enemy) < 5000 then  
+
+               if CanUseSpell(myHero, _R) and RPred.HitChance == 1 and GoS:ValidTarget(enemy, GetCastRange(myHero, _R)) and JinxMenu.Killsteal.R:Value() and RPred.HitChance == 1 and GetCurrentHP(enemy) < GoS:CalcDamage(myHero, enemy, (GetMaxHP(enemy)-GetCurrentHP(enemy))*(0.2+0.05*GetCastLevel(myHero, _R))+(150+100*GetCastLevel(myHero, _R)+GetBonusDmg(myHero))*math.max(0.1, math.min(1, GoS:GetDistance(enemy)/1700))) and GoS:GetDistance(myHero, enemy) < 5000 then  
                CastSkillShot(_R,RPred.PredPos.x,RPred.PredPos.y,RPred.PredPos.z)
 			   end
 		end
-end	 
- 
-function Drawings()
-myHeroPos = GetOrigin(myHero)
-if CanUseSpell(myHero, _W) == READY and DrawingsConfig.DrawW then DrawCircle(myHeroPos.x,myHeroPos.y,myHeroPos.z,GetCastRange(myHero,_W),3,100,0xff00ff00) end
-if CanUseSpell(myHero, _Q) == READY and DrawingsConfig.DrawQ then DrawCircle(myHeroPos.x,myHeroPos.y,myHeroPos.z,GetCastRange(myHero,_Q),3,100,0xff00ff00) end
-if CanUseSpell(myHero, _E) == READY and DrawingsConfig.DrawE then DrawCircle(myHeroPos.x,myHeroPos.y,myHeroPos.z,GetCastRange(myHero,_E),3,100,0xff00ff00) end
-if CanUseSpell(myHero, _R) == READY and DrawingsConfig.DrawR then DrawCircle(myHeroPos.x,myHeroPos.y,myHeroPos.z,GetCastRange(myHero,_R),3,100,0xff00ff00) end
-end
+
+
+if JinxMenu.Drawings.W:Value() then DrawCircle(GoS:myHeroPos().x, GoS:myHeroPos().y, GoS:myHeroPos().z,(GetCastRange(myHero,_W)),3,100,0xff00ff00) end
+if JinxMenu.Drawings.Q:Value() then DrawCircle(GoS:myHeroPos().x, GoS:myHeroPos().y, GoS:myHeroPos().z,700,3,100,0xff00ff00) end
+if JinxMenu.Drawings.E:Value() then DrawCircle(GoS:myHeroPos().x, GoS:myHeroPos().y, GoS:myHeroPos().z,(GetCastRange(myHero,_E)),3,100,0xff00ff00) end
+if JinxMenu.Drawings.R:Value() then DrawCircle(GoS:myHeroPos().x, GoS:myHeroPos().y, GoS:myHeroPos().z,(GetCastRange(myHero,_R)),3,100,0xff00ff00) end
+
+end)
