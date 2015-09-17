a50ef550fc48de216fe0f795cd5766ba5f94b6a4
Zypppy <zypppaliukas@gmail.com>
2015-09-17 12:13:22 +0300

    Update Kassadin.lua

--- Kassadin.lua
+++ Kassadin.lua
@@ -1,75 +1,52 @@
-Config = scriptConfig("Kassadin", "Kassadin:")
-Config.addParam("Q", "Use Q", SCRIPT_PARAM_ONOFF, true)
-Config.addParam("W", "Use W", SCRIPT_PARAM_ONOFF, true)
-Config.addParam("E", "Use E", SCRIPT_PARAM_ONOFF, true)
-Config.addParam("R", "Use R", SCRIPT_PARAM_ONOFF, true)
-DrawingsConfig = scriptConfig("Drawings", "Drawings:")
-DrawingsConfig.addParam("DrawQ","Draw Q", SCRIPT_PARAM_ONOFF, true)
-DrawingsConfig.addParam("DrawE","Draw E", SCRIPT_PARAM_ONOFF, true)
-DrawingsConfig.addParam("DrawR","Draw R", SCRIPT_PARAM_ONOFF, true)
-Config.addParam("DMG", "DMG", SCRIPT_PARAM_ONOFF, true)
+KassadinMenu = Menu("Kassadin", "Kassadin")
+KassadinMenu:SubMenu("Combo", "Combo")
+KassadinMenu.Combo:Boolean("Q", "Use Q", true)
+KassadinMenu.Combo:Boolean("W", "Use W", true)
+KassadinMenu.Combo:Boolean("E", "Use E", true)
+KassadinMenu.Combo:Boolean("R", "Use R", true)
+
+KassadinMenu:SubMenu("Drawings", "Drawings:")
+KassadinMenu.Drawings:Boolean("Q","Draw Q", true)
+KassadinMenu.Drawings:Boolean("E","Draw E", true)
+KassadinMenu.Drawings:Boolean("R","Draw R", true)
 
 
  
- 
-myIAC = IAC()
  
 OnLoop(function(myHero)
-Drawings()
+
+
 local target = GetCurrentTarget()
 
- 
-        if IWalkConfig.Combo then
-              local target = GetTarget(1000, DAMAGE_MAGICAL)
-                       
-                        local RPred = GetPredictionForPlayer(GetMyHeroPos(),target,GetMoveSpeed(target),0,250,700,270,false,false)
-                        if CanUseSpell(myHero, _R) == READY and RPred.HitChance == 1 and ValidTarget(target, GetCastRange(myHero,_R)) and Config.R then
+
+
+if IOW:Mode() == "Combo" then
+  
+                        local RPred = GetPredictionForPlayer(GoS:myHeroPos(),target,GetMoveSpeed(target),0,250,700,270,false,false)
+                        local EPred = GetPredictionForPlayer(GoS:myHeroPos(),target,GetMoveSpeed(target),0,250,700,270,false,true)
+
+
+
+
+
+                        if CanUseSpell(myHero, _R) == READY and RPred.HitChance == 1 and GoS:ValidTarget(target, GetCastRange(myHero,_R)) and KassadinMenu.Combo.R:Value() then
                         CastSkillShot(_R,RPred.PredPos.x,RPred.PredPos.y,RPred.PredPos.z)
 						end
-						if CanUseSpell(myHero, _W) == READY and ValidTarget(target, 300) and GetDistance(myHero, target) < 300 and Config.W then
+						if CanUseSpell(myHero, _W) == READY and GoS:ValidTarget(target, 300) and KassadinMenu.Combo.W:Value() then
                         CastSpell(_W)
-						end
-                        local EPred = GetPredictionForPlayer(GetMyHeroPos(),target,GetMoveSpeed(target),0,250,700,270,false,true)
-                        if CanUseSpell(myHero, _E) == READY and EPred.HitChance == 1 and ValidTarget(target, 700) and GetDistance(myHero, target) > 0 and Config.E then
+                    end
+                        if CanUseSpell(myHero, _E) == READY and EPred.HitChance == 1 and GoS:ValidTarget(target, 700) and KassadinMenu.Combo.E:Value() then
                         CastSkillShot(_E,EPred.PredPos.x,EPred.PredPos.y,EPred.PredPos.z)
-end
-						if CanUseSpell(myHero, _Q) == READY and ValidTarget(target, GetCastRange(myHero,_Q)) and Config.Q then
+                        end
+						if CanUseSpell(myHero, _Q) == READY and GoS:ValidTarget(target, GetCastRange(myHero,_Q)) and KassadinMenu.Combo.Q:Value() then
 						CastTargetSpell(target, _Q)
 						end
         end
 
-        if ValidTarget(target, 2000) and Config.DMG then
-  if CanUseSpell(myHero,_Q) == READY then
-local trueDMG = CalcDamage(myHero, target, 0, (25*GetCastLevel(myHero,_Q) + 50 + 0.7*(GetBonusAP(myHero))))
-    DrawDmgOverHpBar(target,GetCurrentHP(target),trueDMG,0,0xff0cff00)
-    end
 
-if CanUseSpell(myHero,_W) == READY then
-local trueDMG = CalcDamage(myHero, target, 0, (25*GetCastLevel(myHero,_W) + 15 + 0.6*(GetBonusAP(myHero))))
-    DrawDmgOverHpBar(target,GetCurrentHP(target),trueDMG,0,0xff0cff00)
-    end
+if KassadinMenu.Drawings.Q:Value() then DrawCircle(GoS:myHeroPos().x, GoS:myHeroPos().y, GoS:myHeroPos().z,(GetCastRange(myHero,_Q)),3,100,0xff00ff00) end
+if KassadinMenu.Drawings.E:Value() then DrawCircle(GoS:myHeroPos().x, GoS:myHeroPos().y, GoS:myHeroPos().z,600,3,100,0xff00ff00) end
+if KassadinMenu.Drawings.R:Value() then DrawCircle(GoS:myHeroPos().x, GoS:myHeroPos().y, GoS:myHeroPos().z,(GetCastRange(myHero,_R)),3,100,0xff00ff00) end
 
-    if CanUseSpell(myHero,_E) == READY then
-local trueDMG = CalcDamage(myHero, target, 0, (25*GetCastLevel(myHero,_E) + 55 + 0.7*(GetBonusAP(myHero))))
-    DrawDmgOverHpBar(target,GetCurrentHP(target),trueDMG,0,0xff0cff00)
-    end
-
-    if CanUseSpell(myHero,_R) == READY then
-local trueDMG = CalcDamage(myHero, target, 0, (20*GetCastLevel(myHero,_R) + 60 + 0.2*(GetBonusAP(myHero))))
-    DrawDmgOverHpBar(target,GetCurrentHP(target),trueDMG,0,0xff0cff00)
-    end
-
-
- end
- 
 
 end)
- 
-function Drawings()
-myHeroPos = GetOrigin(myHero)
-if CanUseSpell(myHero, _Q) == READY and DrawingsConfig.DrawQ then DrawCircle(myHeroPos.x,myHeroPos.y,myHeroPos.z,GetCastRange(myHero,_Q),3,100,0xff00ff00) end
-myHeroPos = GetOrigin(myHero)
-if CanUseSpell(myHero, _E) == READY and DrawingsConfig.DrawE then DrawCircle(myHeroPos.x,myHeroPos.y,myHeroPos.z,GetCastRange(myHero,_E),3,100,0xff00ff00) end
-myHeroPos = GetOrigin(myHero)
-if CanUseSpell(myHero, _R) == READY and DrawingsConfig.DrawR then DrawCircle(myHeroPos.x,myHeroPos.y,myHeroPos.z,GetCastRange(myHero,_R),3,100,0xff00ff00) end
-end
