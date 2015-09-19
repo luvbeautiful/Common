d77777cc665c9baf6f39bc95088aefac9fe4baad
Zypppy <zypppaliukas@gmail.com>
2015-09-19 21:46:19 +0300

    Update Lux.lua

--- Lux.lua
+++ Lux.lua
@@ -1,59 +1,79 @@
-Config = scriptConfig("Lux", "Lux:")
-Config.addParam("Q", "Use Q", SCRIPT_PARAM_ONOFF, true)
-Config.addParam("W", "Use W", SCRIPT_PARAM_ONOFF, true)
-Config.addParam("E", "Use E", SCRIPT_PARAM_ONOFF, true)
-DrawingsConfig = scriptConfig("Drawings", "Drawings:")
-KSConfig = scriptConfig("KS", "Killsteal:")
-KSConfig.addParam("KSR", "Killsteal with R", SCRIPT_PARAM_ONOFF, false)
-DrawingsConfig.addParam("DrawQ","Draw Q", SCRIPT_PARAM_ONOFF, true)
-DrawingsConfig.addParam("DrawW","Draw W", SCRIPT_PARAM_ONOFF, true)
-DrawingsConfig.addParam("DrawE","Draw E", SCRIPT_PARAM_ONOFF, true)
-Config.addParam("DMG", "DMG", SCRIPT_PARAM_ONOFF, true)
+LuxMenu = Menu("Lux", "Lux:")
+LuxMenu:SubMenu("Combo", "Combo")
+LuxMenu.Combo:Boolean("Q", "Use Q", true)
+LuxMenu.Combo:Boolean("E", "Use E", true)
+LuxMenu.Combo:Boolean("AW", "Use Ally W", true)
+LuxMenu.Combo:Boolean("MW", "Use Me W", true)
+
+LuxMenu:SubMenu("Killsteal", "Killsteal")
+LuxMenu.Killsteal:Boolean("R", "Killsteal with R", true)
 
 
- 
- 
-myIAC = IAC()
- 
+LuxMenu:SubMenu("Drawings", "Drawings:")
+LuxMenu.Drawings:Boolean("Q", "Draw Q", true)
+LuxMenu.Drawings:Boolean("W", "Draw W", true)
+LuxMenu.Drawings:Boolean("E", "Draw E", true)
+LuxMenu.Drawings:Boolean("R", "Draw R", true)
+
+
+LuxMenu:SubMenu("DMG", "Draw DMG")
+LuxMenu.DMG:Boolean("Q", "Draw Q Dmg", true)
+LuxMenu.DMG:Boolean("E", "Draw E Dmg", true)
+LuxMenu.DMG:Boolean("R", "Draw R Dmg", true)
+
+
+
 OnLoop(function(myHero)
-Drawings()
-Killsteal()
+
+
 local target = GetCurrentTarget()
- 
-        if IWalkConfig.Combo then
-              local target = GetTarget(1500, DAMAGE_MAGIC)
-				
-				        local QPred = GetPredictionForPlayer(GetMyHeroPos(),target,GetMoveSpeed(target),1200,250,1300,70,true,false)
-                        if CanUseSpell(myHero, _Q) == READY and QPred.HitChance == 1 and ValidTarget(target, GetCastRange(myHero,_Q)) and Config.Q then
+
+
+
+if IOW:Mode() == "Combo" then
+
+
+
+
+				local QPred = GetPredictionForPlayer(GoS:myHeroPos(),target,GetMoveSpeed(target),1200,250,1300,70,true,false)       
+        local EPred = GetPredictionForPlayer(GoS:myHeroPos(),target,GetMoveSpeed(target),1300,250,1100,300,false,false)
+        local RPred = GetPredictionForPlayer(GoS:myHeroPos(),target,GetMoveSpeed(target),0,1000,3500,110,false,false)
+
+
+
+
+
+                        if CanUseSpell(myHero, _Q) == READY and QPred.HitChance == 1 and GoS:ValidTarget(target, GetCastRange(myHero,_Q)) and LuxMenu.Combo.Q:Value() then
                         CastSkillShot(_Q,QPred.PredPos.x,QPred.PredPos.y,QPred.PredPos.z)
 					           	end
-                      local WPred = GetPredictionForPlayer(GetMyHeroPos(),target,GetMoveSpeed(target),1600,250,1050,80,false,false)
-                      if (GetCurrentHP(myHero)/GetMaxHP(myHero))<0.5 and CanUseSpell(myHero, _W) == READY and WPred.HitChance == 1 and ValidTarget(target, GetCastRange(myHero,_W)) and Config.W then
+                      for _, ally in pairs(GoS:GetAllyHeroes()) do
+                        local WPred = GetPredictionForPlayer(GoS:myHeroPos(),ally,GetMoveSpeed(ally),1600,250,1050,80,false,false)
+                        if CanUseSpell(myHero, _W) == READY and WPred.HitChance == 1 and (GetCurrentHP(ally)/GetMaxHP(ally))<0.5 and GoS:ValidTarget(target, GetCastRange(myHero,_W)) and LuxMenu.Combo.AW:Value() then
+                           CastSkillShot(_W,WPred.PredPos.x,WPred.PredPos.y,WPred.PredPos.z)
+                         end
+                       end
+                       local WPred = GetPredictionForPlayer(GoS:myHeroPos(),myHero,GetMoveSpeed(myHero),1600,250,1050,80,false,false)
+                      if CanUseSpell(myHero, _W) == READY and WPred.HitChance == 1 and (GetCurrentHP(myHero)/GetMaxHP(myHero))<0.5 and GoS:ValidTarget(target, GetCastRange(myHero,_W)) and LuxMenu.Combo.MW:Value() then
                         CastSkillShot(_W,WPred.PredPos.x,WPred.PredPos.y,WPred.PredPos.z)
                      end
-						local EPred = GetPredictionForPlayer(GetMyHeroPos(),target,GetMoveSpeed(target),1300,300,1100,300,false,true)
-                        if CanUseSpell(myHero, _E) == READY and EPred.HitChance == 1 and ValidTarget(target, GetCastRange(myHero,_E)) and Config.E then
+                        if CanUseSpell(myHero, _E) == READY and EPred.HitChance == 1 and GoS:ValidTarget(target, GetCastRange(myHero,_E)) and LuxMenu.Combo.E:Value() then
                         CastSkillShot(_E,EPred.PredPos.x,EPred.PredPos.y,EPred.PredPos.z)
 						end
-						local RPred = GetPredictionForPlayer(GetMyHeroPos(),target,GetMoveSpeed(target),0,1000,3500,110,false,true)
-                        if CanUseSpell(myHero, _R) == READY and RPred.HitChance == 1 and ValidTarget(target, GetCastRange(myHero,_R)) and Config.R then
-                        CastSkillShot(_R,RPred.PredPos.x,RPred.PredPos.y,RPred.PredPos.z)
-					             end 
         end
 
-        if ValidTarget(target, 2000) and Config.DMG then
-  if CanUseSpell(myHero,_Q) == READY then
-local trueDMG = CalcDamage(myHero, target, 0, (50*GetCastLevel(myHero,_Q) + 10 + 0.7*(GetBonusAP(myHero))))
-    DrawDmgOverHpBar(target,GetCurrentHP(target),trueDMG,0,0xff0cff00)
-    end
 
-    if CanUseSpell(myHero,_E) == READY then
-local trueDMG = CalcDamage(myHero, target, 0, (45*GetCastLevel(myHero,_E) + 15 + 0.6*(GetBonusAP(myHero))))
-    DrawDmgOverHpBar(target,GetCurrentHP(target),trueDMG,0,0xff0cff00)
-    end
 
-    if CanUseSpell(myHero,_R) == READY then
-local trueDMG = CalcDamage(myHero, target, 0, (100*GetCastLevel(myHero,_R) + 200 + 0.75*(GetBonusAP(myHero))))
+
+for i,enemy in pairs(GoS:GetEnemyHeroes()) do
+        
+  if CanUseSpell(myHero,_Q) == READY and LuxMenu.DMG.Q:Value() and GoS:ValidTarget(target, GetCastRange(myHero,_Q)) then
+local trueDMG = GoS:CalcDamage(myHero, target, 0, (50*GetCastLevel(myHero,_Q) + 10 + 0.7*(GetBonusAP(myHero))))
+    DrawDmgOverHpBar(target,GetCurrentHP(target),trueDMG,0,0xff0cff00)
+    elseif CanUseSpell(myHero,_E) == READY and LuxMenu.DMG.E:Value() and GoS:ValidTarget(target, GetCastRange(myHero,_E)) then
+local trueDMG = GoS:CalcDamage(myHero, target, 0, (45*GetCastLevel(myHero,_E) + 15 + 0.6*(GetBonusAP(myHero))))
+    DrawDmgOverHpBar(target,GetCurrentHP(target),trueDMG,0,0xff0cff00)
+    elseif CanUseSpell(myHero,_R) == READY and LuxMenu.DMG.R:Value() and GoS:ValidTarget(target, GetCastRange(myHero,_R)) then
+local trueDMG = GoS:CalcDamage(myHero, target, 0, (100*GetCastLevel(myHero,_R) + 200 + 0.75*(GetBonusAP(myHero))))
     DrawDmgOverHpBar(target,GetCurrentHP(target),trueDMG,0,0xff0cff00)
     end
 
@@ -61,22 +81,20 @@ local trueDMG = CalcDamage(myHero, target, 0, (100*GetCastLevel(myHero,_R) + 200
  end
  
 
-end)
  
-function Killsteal()
-        for i,enemy in pairs(GetEnemyHeroes()) do
-	           local RPred = GetPredictionForPlayer(GetMyHeroPos(),enemy,GetMoveSpeed(enemy),0,1200,3500,190,false,false)
-  	           if CanUseSpell(myHero, _R) == READY and RPred.HitChance == 1 and ValidTarget(enemy,GetCastRange(myHero,_R)) and KSConfig.KSR and GetCurrentHP(enemy) < CalcDamage(myHero,  enemy, 0, (100*GetCastLevel(myHero,_R) + 200 + 0.75*(GetBonusAP(myHero)))) then 
+for i,enemy in pairs(GoS:GetEnemyHeroes()) do
+ 
+local RPred = GetPredictionForPlayer(Gos:myHeroPos(),enemy,GetMoveSpeed(enemy),0,1200,3500,190,false,false)
+
+
+  	           if CanUseSpell(myHero, _R) == READY and RPred.HitChance == 1 and GoS:ValidTarget(enemy,GetCastRange(myHero,_R)) and LuxMenu.Killsteal.R:Value() and GetCurrentHP(enemy) < GoS:CalcDamage(myHero,  enemy, 0, (100*GetCastLevel(myHero,_R) + 200 + 0.75*(GetBonusAP(myHero)))) then 
               CastSkillShot(_R,RPred.PredPos.x,RPred.PredPos.y,RPred.PredPos.z)
              end
       end
-end
  
- 
-function Drawings()
-myHeroPos = GetOrigin(myHero)
-if CanUseSpell(myHero, _Q) == READY and DrawingsConfig.DrawQ then DrawCircle(myHeroPos.x,myHeroPos.y,myHeroPos.z,GetCastRange(myHero,_Q),3,100,0xff00ff00) end
-if CanUseSpell(myHero, _W) == READY and DrawingsConfig.DrawW then DrawCircle(myHeroPos.x,myHeroPos.y,myHeroPos.z,GetCastRange(myHero,_W),3,100,0xff00ff000) end
-if CanUseSpell(myHero, _E) == READY and DrawingsConfig.DrawE then DrawCircle(myHeroPos.x,myHeroPos.y,myHeroPos.z,GetCastRange(myHero,_E),3,100,0xff00fc00) end
-if CanUseSpell(myHero, _R) == READY and DrawingsConfig.DrawR then DrawCircle(myHeroPos.x,myHeroPos.y,myHeroPos.z,GetCastRange(myHero,_R),3,100,0xff00ffc0) end
-end
+if LuxMenu.Drawings.Q:Value() then DrawCircle(GoS:myHeroPos().x, GoS:myHeroPos().y, GoS:myHeroPos().z,GetCastRange(myHero,_Q),3,100,0xff00ff00) end
+if LuxMenu.Drawings.W:Value() then DrawCircle(GoS:myHeroPos().x, GoS:myHeroPos().y, GoS:myHeroPos().z,GetCastRange(myHero,_E),3,100,0xff00ff00) end
+if LuxMenu.Drawings.E:Value() then DrawCircle(GoS:myHeroPos().x, GoS:myHeroPos().y, GoS:myHeroPos().z,GetCastRange(myHero,_W),3,100,0xff00ff00) end
+if LuxMenu.Drawings.R:Value() then DrawCircle(GoS:myHeroPos().x, GoS:myHeroPos().y, GoS:myHeroPos().z,GetCastRange(myHero,_R),3,100,0xff00ff00) end
+
+end)
