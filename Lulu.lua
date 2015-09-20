8d64e524d6b9dde3096c34802eda46e4529175b8
Zypppy <zypppaliukas@gmail.com>
2015-09-20 12:50:50 +0300

    Update Lulu.lua

--- Lulu.lua
+++ Lulu.lua
@@ -18,40 +18,43 @@ LuluMenu.Drawings:Boolean("R", "Draw R", true)
 LuluMenu:SubMenu("DMG", "Draw DMG")
 LuluMenu.DMG:Boolean("Q", "Draw Q Dmg", true)
 LuluMenu.DMG:Boolean("E", "Draw E Dmg", false)
+
+ 
+ 
  
 OnLoop(function(myHero)
-
-
-local target = GetCurrentTarget()
-
-
-
-if IOW:Mode() == "Combo" then
- local QPred = GetPredictionForPlayer(GoS:myHeroPos(),target,GetMoveSpeed(target),1450,250,925,80,false,false)
+ 
+        if IOW:Mode() == "Combo" then
+                       
+                        local target = GetCurrentTarget()
+                      local QPred = GetPredictionForPlayer(GoS:myHeroPos(),target,GetMoveSpeed(target),1450,250,925,80,false,false)
 
 
                        
 					   
-                        if CanUseSpell(myHero, _Q) == READY and QPred.HitChance == 1 and GoS:ValidTarget(target, GetCastRange(myHero,_Q)) and LuluMenu.Combo.Q:Value() then
+                        if CanUseSpell(myHero, _Q) == READY and QPred.HitChance == 1 and GoS:ValidTarget(target, 1300) and LuluMenu.Combo.Q:Value() then
                         CastSkillShot(_Q,QPred.PredPos.x,QPred.PredPos.y,QPred.PredPos.z)
 						end
-                        if CanUseSpell(myHero, _W) == READY and GoS:ValidTarget(target, GetCastRange(myHero,_W)) and LuluMenu.Combo.W:Value() then
+                        if CanUseSpell(myHero, _W) == READY and GoS:ValidTarget(target, 700) and LuluMenu.Combo.W:Value() then
                         CastTargetSpell(target, _W)
                         end
-                        for _, ally in pairs(GoS:GetAllyHeroes()) do
-					if CanUseSpell(myHero, _E) == READY and GoS:ValidTarget(target, GetCastRange(myHero,_E)) and LuluMenu.Combo.EE:Value() then
-                     CastTargetSpell(target, _E)
-                       elseif CanUseSpell(myHero, _E) == READY and GoS:ValidTarget(target, 1000) and (GetCurrentHP(ally)/GetMaxHP(ally))<0.3 and LuluMenu.Combo.AE:Value() then
-                      CastTargetSpell(ally, _E)
-                       elseif CanUseSpell(myHero,_E) == READY and GoS:ValidTarget(target, 1000) and (GetCurrentHP(myHero)/GetMaxHP(myHero))<0.15 and LuluMenu.Combo.ME:Value() then
-                         CastTargetSpell(myHero, _E)   
-						end  
+					    if CanUseSpell(myHero, _E) == READY and GoS:ValidTarget(target, 1000) and LuluMenu.Combo.EE:Value() then
+                        CastTargetSpell(target, _E)
                         end
-                        end 
+                        for _, ally in pairs(GoS:GetAllyHeroes()) do
+                        if CanUseSpell(myHero, _E) == READY and GoS:ValidTarget(target, 1000) and (GetCurrentHP(ally)/GetMaxHP(ally))<0.3 and LuluMenu.Combo.AE:Value() then
+                        CastTargetSpell(ally, _E)
+                        end
+                    end
+                        if CanUseSpell(myHero,_E) == READY and GoS:ValidTarget(target, 1000) and (GetCurrentHP(myHero)/GetMaxHP(myHero))<0.15 and LuluMenu.Combo.ME:Value() then
+                        CastTargetSpell(myHero, _E)   
+						end
                         for _, ally in pairs(GoS:GetAllyHeroes()) do
                         if LuluMenu.Combo.AR:Value() and GoS:ValidTarget(target, 1000) and (GetCurrentHP(ally)/GetMaxHP(ally))<0.3 and CanUseSpell(myHero, _R) == READY then
                         CastTargetSpell(ally, _R)
-                        elseif LuluMenu.Combo.MR:Value() and GoS:ValidTarget(target, 1000) and (GetCurrentHP(myHero)/GetMaxHP(myHero))<0.3 and CanUseSpell(myHero, _R) == READY then
+                        end
+                    end
+                        if LuluMenu.Combo.MR:Value() and GoS:ValidTarget(target, 1000) and (GetCurrentHP(myHero)/GetMaxHP(myHero))<0.3 and CanUseSpell(myHero, _R) == READY then
                         CastTargetSpell(myHero, _R)
                         end
 
@@ -60,13 +63,13 @@ if IOW:Mode() == "Combo" then
 
 for i,enemy in pairs(GoS:GetEnemyHeroes()) do
 
-  if CanUseSpell(myHero,_Q) == READY and LuluMenu.DMG.Q:Value() then
-local trueDMG = GoS:CalcDamage(myHero, target, 0, (45*GetCastLevel(myHero,_Q) + 35 + 0.5*(GetBonusAP(myHero))))
-    DrawDmgOverHpBar(target,GetCurrentHP(target),trueDMG,0,0xff0cff00)
-
-elseif CanUseSpell(myHero,_E) == READY and LuluMenu.DMG.E:Value() then 
-local trueDMG = GoS:CalcDamage(myHero, target, 0, (30*GetCastLevel(myHero,_E) + 50 + 0.4*(GetBonusAP(myHero))))
-    DrawDmgOverHpBar(target,GetCurrentHP(target),trueDMG,0,0xff0cff00)
+if CanUseSpell(myHero,_Q) == READY and GoS:ValidTarget(enemy, 1300) and LuluMenu.DMG.Q:Value() then
+local trueDMG = GoS:CalcDamage(myHero, enemy, 0, (45*GetCastLevel(myHero,_Q) + 35 + 0.5*(GetBonusAP(myHero))))
+    DrawDmgOverHpBar(enemy,GetCurrentHP(enemy),trueDMG,0,0xff0cff00)
+end
+if CanUseSpell(myHero,_E) == READY and GoS:ValidTarget(enemy, 1300) and LuluMenu.DMG.E:Value() then 
+local trueDMG = GoS:CalcDamage(myHero, enemy, 0, (30*GetCastLevel(myHero,_E) + 50 + 0.4*(GetBonusAP(myHero))))
+    DrawDmgOverHpBar(enemy,GetCurrentHP(enemy),trueDMG,0,0xff0cff00)
   end
     
 end
@@ -79,6 +82,5 @@ end
 if LuluMenu.Drawings.Q:Value() then DrawCircle(GoS:myHeroPos().x, GoS:myHeroPos().y, GoS:myHeroPos().z,GetCastRange(myHero,_Q),3,100,0xff00ff00) end
 if LuluMenu.Drawings.E:Value() then DrawCircle(GoS:myHeroPos().x, GoS:myHeroPos().y, GoS:myHeroPos().z,GetCastRange(myHero,_E),3,100,0xff00ff00) end
 if LuluMenu.Drawings.W:Value() then DrawCircle(GoS:myHeroPos().x, GoS:myHeroPos().y, GoS:myHeroPos().z,GetCastRange(myHero,_W),3,100,0xff00ff00) end
-if LuluMenu.Drawings.R:Value() then DrawCircle(GoS:myHeroPos().x, GoS:myHeroPos().y, GoS:myHeroPos().z,GetCastRange(myHero,_R),3,100,0xff00ff00) end
 
 end)
