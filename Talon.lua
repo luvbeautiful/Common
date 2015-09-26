646a6618226a56bad2e928f331808542d24d9e7c
Zypppy <zypppaliukas@gmail.com>
2015-09-27 02:12:19 +0300

    R when <=2 enemies or  when  you below  40% hp  or  when your  R will kill them

--- Talon.lua
+++ Talon.lua
@@ -1,76 +1,68 @@
-MorganaMenu = Menu("Talon", "Talon")
-MorganaMenu:SubMenu("Combo", "Combo")
-MorganaMenu.Combo:Boolean("Q", "Use Q", true)
-MorganaMenu.Combo:Boolean("W", "Use W", false)
-MorganaMenu.Combo:Boolean("E", "Use E", true)
-MorganaMenu.Combo:Boolean("R", "Use R", true)
+TalonMenu = Menu("Talon", "Talon")
+TalonMenu:SubMenu("Combo", "Combo")
+TalonMenu.Combo:Boolean("Q", "Use Q", true)
+TalonMenu.Combo:Boolean("W", "Use W", true)
+TalonMenu.Combo:Boolean("E", "Use E", true)
+TalonMenu.Combo:Boolean("R", "Use R", true)
 
-MorganaMenu:SubMenu("Drawings", "Drawings:")
-MorganaMenu.Drawings:Boolean("W", "Draw W", true)
-MorganaMenu.Drawings:Boolean("E", "Draw E", true)
-
-
-MorganaMenu:SubMenu("DMG", "Draw DMG")
-MorganaMenu.DMG:Boolean("Q", "Draw Q Dmg", true)
-MorganaMenu.DMG:Boolean("W", "Draw W Dmg", true)
-MorganaMenu.DMG:Boolean("R", "Draw R Dmg", true)
+TalonMenu:SubMenu("Drawings", "Drawings:")
+TalonMenu.Drawings:Boolean("W", "Draw W", true)
+TalonMenu.Drawings:Boolean("E", "Draw E", false)
+TalonMenu.Drawings:Boolean("R", "Draw R", false)
 
 
-
-Config = scriptConfig("Talon", "Talon:")
-Config.addParam("Q", "Use Q", SCRIPT_PARAM_ONOFF, true)
-Config.addParam("W", "Use W", SCRIPT_PARAM_ONOFF, true)
-Config.addParam("E", "Use E", SCRIPT_PARAM_ONOFF, true)
-Config.addParam("R", "Use R", SCRIPT_PARAM_ONOFF, true)
-DrawingsConfig = scriptConfig("Drawings", "Drawings:")
-DrawingsConfig.addParam("DrawW","Draw W", SCRIPT_PARAM_ONOFF, true)
-DrawingsConfig.addParam("DrawE","Draw E", SCRIPT_PARAM_ONOFF, true)
-Config.addParam("DMG", "DMG", SCRIPT_PARAM_ONOFF, true)
+TalonMenu:SubMenu("DMG", "Draw DMG")
+TalonMenu.DMG:Boolean("W", "Draw W Dmg", true)
+TalonMenu.DMG:Boolean("R", "Draw R Dmg", true)
 
 
  
- 
-myIAC = IAC()
  
 OnLoop(function(myHero)
-Drawings()
-local target = GetCurrentTarget()
 
- 
-        if IWalkConfig.Combo then
-              local target = GetTarget(750, DAMAGE_PHYSICAL)
+  
+
+
+           if  IOW:Mode() == "Combo" then
+                     local target = GetCurrentTarget()
+                     local WPred = GetPredictionForPlayer(GoS:myHeroPos(),target,GetMoveSpeed(target),1200,250,600,200,false,true)
 				
-				        if CanUseSpell(myHero, _Q) == READY and IsInDistance(target, 500) and Config.Q then
+				        if CanUseSpell(myHero, _Q) == READY and GoS:ValidTarget(target, 300) and TalonMenu.Combo.Q:Value() then
                         CastSpell(_Q)
                         end
-						local WPred = GetPredictionForPlayer(GetMyHeroPos(),target,GetMoveSpeed(target),2300,0,780,75,false,true)
-                        if CanUseSpell(myHero, _W) == READY and WPred.HitChance == 1 and ValidTarget(target, 780) and Config.W then
+                        if CanUseSpell(myHero, _W) == READY and WPred.HitChance == 1 and GoS:ValidTarget(target, 1000) and GoS:GetDistance(myHero, target) <= 600 and TalonMenu.Combo.W:Value() then
                         CastSkillShot(_W,WPred.PredPos.x,WPred.PredPos.y,WPred.PredPos.z)
 						end
-						if CanUseSpell(myHero, _E) == READY and ValidTarget(target, GetCastRange(myHero,_E)) and Config.E then
+						if CanUseSpell(myHero, _E) == READY and GoS:ValidTarget(target, 600) and TalonMenu.Combo.E:Value() then
                         CastTargetSpell(target, _E)
 						end
-						if CanUseSpell(myHero, _R) == READY and ValidTarget(target, GetCastRange(myHero,_E)) and GetDistance(myHero, target) < 750 and Config.R then
+                        for i,enemy in pairs(GoS:GetEnemyHeroes()) do
+						if CanUseSpell(myHero, _R) == READY and GoS:ValidTarget(target, 1000) and GoS:GetDistance(myHero, target) <= 680 and GoS:EnemiesAround(GoS:myHeroPos(), 680) >=2 or (GetCurrentHP(myHero)/GetMaxHP(myHero))<=0.40  and TalonMenu.Combo.R:Value() then
                         CastSpell(_R)
 						end
+                        end
+                        for i,enemy in pairs(GoS:GetEnemyHeroes()) do
+                        if CanUseSpell(myHero, _R) == READY and GoS:ValidTarget(enemy, 1000) and GoS:GetDistance(myHero, enemy) <= 680 and GetCurrentHP(enemy) < GoS:CalcDamage(myHero, enemy, (50*GetCastLevel(myHero,_R) + 70 + 0.75*(GetBaseDamage(myHero) + GetBonusDmg(myHero))),0) and TalonMenu.Combo.R:Value() then
+                        CastSpell(_R)
+                        end
+                        end
         end
 
-
- if CanUseSpell(myHero,_W) == READY and ValidTarget(target, 2000) and Config.DMG then
-local trueDMG = CalcDamage(myHero, target, 0, (25*GetCastLevel(myHero,_W) + 5 + 0.6*(GetBaseDamage(myHero) + GetBonusDmg(myHero))))
-    DrawDmgOverHpBar(target,GetCurrentHP(target),trueDMG,0,0xff0cff00)
-     elseif CanUseSpell(myHero,_R) == READY then
-local trueDMG = CalcDamage(myHero, target, 0, (50*GetCastLevel(myHero,_R) + 70 + 0.75*(GetBaseDamage(myHero) + GetBonusDmg(myHero))))
-    DrawDmgOverHpBar(target,GetCurrentHP(target),trueDMG,0,0xff0cff00)
+for i,enemy in pairs(GoS:GetEnemyHeroes()) do
+ if CanUseSpell(myHero,_W) == READY and GoS:ValidTarget(enemy, 2000) and TalonMenu.DMG.W:Value() then
+local trueDMG = GoS:CalcDamage(myHero, enemy, (25*GetCastLevel(myHero,_W) + 5 + 0.6*(GetBaseDamage(myHero) + GetBonusDmg(myHero))),0)
+    DrawDmgOverHpBar(enemy,GetCurrentHP(enemy),trueDMG,0,0xff0cff00)
+     elseif CanUseSpell(myHero,_R) == READY and TalonMenu.DMG.R:Value() then
+local trueDMG = GoS:CalcDamage(myHero, enemy, (50*GetCastLevel(myHero,_R) + 70 + 0.75*(GetBaseDamage(myHero) + GetBonusDmg(myHero))),0)
+    DrawDmgOverHpBar(enemy,GetCurrentHP(enemy),trueDMG,0,0xff0cff00)
     end
 
  end
  
 
-end)
  
-function Drawings()
-myHeroPos = GetOrigin(myHero)
-if CanUseSpell(myHero, _W) == READY and DrawingsConfig.DrawW then DrawCircle(myHeroPos.x,myHeroPos.y,myHeroPos.z,GetCastRange(myHero,_W),3,100,0xff00ff00) end
-if CanUseSpell(myHero, _E) == READY and DrawingsConfig.DrawE then DrawCircle(myHeroPos.x,myHeroPos.y,myHeroPos.z,GetCastRange(myHero,_E),3,100,0xff00ffc00) end
-end
+if TalonMenu.Drawings.W:Value() then DrawCircle(GoS:myHeroPos().x, GoS:myHeroPos().y, GoS:myHeroPos().z,600,3,100,0xff00ff00) end
+if TalonMenu.Drawings.E:Value() then DrawCircle(GoS:myHeroPos().x, GoS:myHeroPos().y, GoS:myHeroPos().z,600,3,100,0xff00ff00) end
+if TalonMenu.Drawings.R:Value() then DrawCircle(GoS:myHeroPos().x, GoS:myHeroPos().y, GoS:myHeroPos().z,650,3,100,0xff00ff00) end
+
+end)
