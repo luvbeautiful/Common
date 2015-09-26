300bc4cca209edd833794e39162529c1b590a0f3
Zypppy <zypppaliukas@gmail.com>
2015-09-26 22:54:10 +0300

    Update Soraka.lua

--- Soraka.lua
+++ Soraka.lua
@@ -1,55 +1,57 @@
-Config = scriptConfig("Soraka", "Soraka:")
-Config.addParam("Q", "Use Q", SCRIPT_PARAM_ONOFF, true)
-Config.addParam("E", "Use E", SCRIPT_PARAM_ONOFF, true)
-Config.addParam("W", "Use W", SCRIPT_PARAM_ONOFF, true)
-Config.addParam("R", "Use R", SCRIPT_PARAM_ONOFF, true)
-DrawingsConfig = scriptConfig("Drawings", "Drawings:")
-DrawingsConfig.addParam("DrawQ","Draw Q", SCRIPT_PARAM_ONOFF, true)
-DrawingsConfig.addParam("DrawE","Draw E", SCRIPT_PARAM_ONOFF, true)
+SorakaMenu = Menu("Soraka", "Soraka")
+SorakaMenu:SubMenu("Combo", "Combo")
+SorakaMenu.Combo:Boolean("Q", "Use Q", true)
+SorakaMenu.Combo:Boolean("W", "Use W", true)
+SorakaMenu.Combo:Boolean("E", "Use E", true)
+SorakaMenu.Combo:Boolean("R", "Use R", true)
 
 
- 
-myIAC = IAC()
- 
+SorakaMenu:SubMenu("Drawings", "Drawings:")
+SorakaMenu.Drawings:Boolean("Q", "Draw Q", true)
+SorakaMenu.Drawings:Boolean("E", "Draw E", false)
+
+
+
 OnLoop(function(myHero)
-Drawings()
 
- 
-        if IWalkConfig.Combo then
-              local target = GetTarget(1150, DAMAGE_MAGIC)
-                if ValidTarget(target, 1150) then
+  
+
+
+           if  IOW:Mode() == "Combo" then
+                     local target = GetCurrentTarget()
                        
-					    local QPred = GetPredictionForPlayer(GetMyHeroPos(),target,GetMoveSpeed(target),1100,300,970,260,false,true)
-                        if CanUseSpell(myHero, _Q) == READY and QPred.HitChance == 1 and ValidTarget(target, GetCastRange(myHero,_Q)) and Config.Q then
+					    local QPred = GetPredictionForPlayer(GoS:myHeroPos(),target,GetMoveSpeed(target),1750,250,1100,210,false,true)
+
+
+                        if CanUseSpell(myHero, _Q) == READY and QPred.HitChance == 1 and GoS:ValidTarget(target, 1100) and SorakaMenu.Combo.Q:Value() then
                         CastSkillShot(_Q,QPred.PredPos.x,QPred.PredPos.y,QPred.PredPos.z)
 						end
-						for _, ally in pairs(GetAllyHeroes()) do
-			            if Config.W then
-			            if (GetCurrentHP(ally)/GetMaxHP(ally))<0.8 and
-	            	    CanUseSpell(myHero, _W) == READY and IsInDistance(ally, 600) then
+						for _, ally in pairs(GoS:GetAllyHeroes()) do
+			            if (GetCurrentHP(ally)/GetMaxHP(ally))<=0.65 and CanUseSpell(myHero, _W) == READY and GoS:GetDistance(myHero, ally) <= 550 and SorakaMenu.Combo.W:Value() then   
 			            CastTargetSpell(ally, _W)
 		                end
-						end
-						end
-						local EPred = GetPredictionForPlayer(GetMyHeroPos(),target,GetMoveSpeed(target),0,300,925,275,false,true)
-                        if CanUseSpell(myHero, _E) == READY and EPred.HitChance == 1 and ValidTarget(target, GetCastRange(myHero,_E)) and Config.E then
+		                end
+		                for i,enemy in pairs(GoS:GetEnemyHeroes()) do
+		                local EPred = GetPredictionForPlayer(GoS:myHeroPos(),target,GetMoveSpeed(target),0,250,925,250,false,true)
+                        if CanUseSpell(myHero, _E) == READY and EPred.HitChance == 1 and  GoS:EnemiesAround(EPred.PredPos, 250) >= 2 and GoS:ValidTarget(target, 925) and SorakaMenu.Combo.E:Value() then
                         CastSkillShot(_E,EPred.PredPos.x,EPred.PredPos.y,EPred.PredPos.z)
 						end
-						for _, ally in pairs(GetAllyHeroes()) do
-			            if Config.R then
-			            if (GetCurrentHP(ally)/GetMaxHP(ally))<0.3 and
-	            	    CanUseSpell(myHero, _R) == READY and IsObjectAlive(ally) then
+					end
+				 		 for i,enemy in pairs(GoS:GetEnemyHeroes()) do
+				 		 	local EPred = GetPredictionForPlayer(GoS:myHeroPos(),enemy,GetMoveSpeed(enemy),0,250,925,250,false,true)
+                        if CanUseSpell(myHero, _E) == READY and GoS:ValidTarget(enemy, 925) and SorakaMenu.Combo.E:Value() and GetCurrentHP(enemy) < GoS:CalcDamage(myHero, enemy, 0, (40*GetCastLevel(myHero,_E) + 30 + 0.4*GetBonusAP(myHero))) then
+                 CastSkillShot(_E,EPred.PredPos.x,EPred.PredPos.y,EPred.PredPos.z)
+             end
+         end
+						for _, ally in pairs(GoS:GetAllyHeroes()) do 
+			            if (GetCurrentHP(ally)/GetMaxHP(ally))<=0.15 and CanUseSpell(myHero, _R) == READY and IsObjectAlive(ally) and SorakaMenu.Combo.R:Value() then
 			            CastSpell(_R)
 						end
 						end
-						end
-                end
-        end
-end)
+end
 
  
-function Drawings()
-myHeroPos = GetOrigin(myHero)
-if CanUseSpell(myHero, _Q) == READY and DrawingsConfig.DrawQ then DrawCircle(myHeroPos.x,myHeroPos.y,myHeroPos.z,GetCastRange(myHero,_Q),3,100,0xff00ff00) end
-if CanUseSpell(myHero, _E) == READY and DrawingsConfig.DrawE then DrawCircle(myHeroPos.x,myHeroPos.y,myHeroPos.z,GetCastRange(myHero,_E),3,100,0xff00ff00) end
-end
+if SorakaMenu.Drawings.Q:Value() then DrawCircle(GoS:myHeroPos().x, GoS:myHeroPos().y, GoS:myHeroPos().z,1300,3,100,0xff00ffff) end
+if SorakaMenu.Drawings.E:Value() then DrawCircle(GoS:myHeroPos().x, GoS:myHeroPos().y, GoS:myHeroPos().z,925,3,100,0xff00ffff) end
+
+end)
