f9d67b4be58e5e6d1996fd76527fc00618fd3634
Zypppy <zypppaliukas@gmail.com>
2015-10-05 10:08:40 +0300

    Update Aatrox.lua

--- Aatrox.lua
+++ Aatrox.lua
@@ -9,8 +9,9 @@ AatroxMenu:SubMenu("Drawings", "Drawings")
 AatroxMenu.Drawings:Boolean("Q", "Draw Q Range", true)
 AatroxMenu.Drawings:Boolean("E", "Draw E Range", true)
 
- 
- 
+AatroxMenu:SubMenu("DMG", "Draw DMG")
+AatroxMenu.DMG:Boolean("Q", "Draw Q Dmg", true)
+AatroxMenu.DMG:Boolean("E", "Draw E Dmg", true) 
  
 OnLoop(function(myHero)
  
@@ -41,6 +42,19 @@ OnLoop(function(myHero)
 						CastSpell(_R)
 						end
         end
+
+
+for i,enemy in pairs(GoS:GetEnemyHeroes()) do
+ if CanUseSpell(myHero,_Q) == READY and GoS:ValidTarget(enemy, 2000) and AatroxMenu.DMG.Q:Value() then
+local trueDMG = GoS:CalcDamage(myHero, enemy, (45*GetCastLevel(myHero,_Q) - 25 + 0.6*(GetBaseDamage(myHero) + GetBonusDmg(myHero))),0)
+    DrawDmgOverHpBar(enemy,GetCurrentHP(enemy),trueDMG,0,0xffffff00)
+     elseif CanUseSpell(myHero,_E) == READY and AatroxMenu.DMG.E:Value() then
+local trueDMG = GoS:CalcDamage(myHero, enemy, 0, (35*GetCastLevel(myHero,_E) + 0.6*(GetBonusAP(myHero) - 40 + 0.6*(GetBaseDamage(myHero) + GetBonusDmg(myHero))),0)
+    DrawDmgOverHpBar(enemy,GetCurrentHP(enemy),trueDMG,0,0xffffff00)
+    end
+
+ end
+
 
 
 if AatroxMenu.Drawings.Q:Value() then DrawCircle(GoS:myHeroPos().x, GoS:myHeroPos().y, GoS:myHeroPos().z,650,3,100,0xffffff00) end
